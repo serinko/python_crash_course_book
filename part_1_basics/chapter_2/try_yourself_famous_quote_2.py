536a5ea1681f746084422493854c2ccebd51f4a0
serinko <johanesxsalmon@riseup.net>
2022-01-14 07:03:51 +0100

    repeating excercise

---
+++ part_1_basics/chapter_2/try_yourself_famous_quote_2.py
@@ -0,0 +1,7 @@
+
+name = "lucy parsons" 
+famous_person = name
+message_validation = "'s wise words are still very valid. Just think about them:"
+message_quote = '\n\t"Anarchists know that a long period of education must \n\tprecede any great fundamental change in society, hence they \n\tdo not believe in vote begging, nor political campaigns, \n\tbut rather in the development of self-thinking individuals."'
+
+print(famous_person.title() + message_validation + message_quote)
