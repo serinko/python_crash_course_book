0a528cb99345d427a0771c4524c29c33e49117e2
serinko <johanesxsalmon@riseup.net>
2022-01-14 05:28:49 +0100

    string edits

--- part_1_basics/chapter_2/whitespaces_in_strings.py
+++ part_1_basics/chapter_2/whitespaces_in_strings.py
@@ -2,3 +2,10 @@ print("Python - normally printed")
 
 print("\tPython - printed with a tab")
 
+print("\n")
+
+print("Languages:\nPython\nC\nJavaScript")
+
+print("\n")
+
+print("Languages:\n\tPython\n\tC\n\tJavaScript")
