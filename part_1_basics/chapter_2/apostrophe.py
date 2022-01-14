142faa494a5be4052b210225fde92f578eceaf68
serinko <johanesxsalmon@riseup.net>
2022-01-14 06:18:25 +0100

    editted  apostrophe.py

--- part_1_basics/chapter_2/apostrophe.py
+++ part_1_basics/chapter_2/apostrophe.py
@@ -1,1 +1,9 @@
+message = "One Python's strength is its diverse community."
+print(message)
+
+print("\n")
+
+message_with_error = 'One Python's strength is its diverse community.'
+print(message_with_error)
+
 
