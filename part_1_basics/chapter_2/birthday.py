175e79e424b765570bb9d8a95b0debc9f5349c15
serinko <johanesxsalmon@riseup.net>
2022-01-17 16:38:11 +0100

    birthday.py, add variables

--- part_1_basics/chapter_2/birthday.py
+++ part_1_basics/chapter_2/birthday.py
@@ -3,9 +3,10 @@ message = "Happy " + str(age) + "rd Birthday"
 
 print(message)
 
+print("\n")
 
 test_result = 5
 name = 'MARTIN'
-message = "Hey, " + name.title() + " your test result is" + str(test_result) + ". Have a good day, Python"
+message = "Hey " + name.title() + " your test result is " + str(test_result) + ". Have a good day, Python"
 
 print(message)
