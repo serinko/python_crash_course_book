157fd2a772a4a1fa2beffe7f70d68acc140d1131
serinko <johanesxsalmon@riseup.net>
2022-01-18 18:34:03 +0100

    edits

--- part_1_basics/chapter_2/try_yourself_adding_comments.py
+++ part_1_basics/chapter_2/try_yourself_adding_comments.py
@@ -1,6 +1,6 @@
 # variables definition
 my_name = "serinko"
-famous_person = "lucy parsons" 
+famouse_person = "lucy parsons" 
 message_validation = "'s wise words are still very valid. Just think about them:"
 message_quote = '\n\t"Anarchists know that a long period of education must \n\tprecede any great fundamental change in society, hence they \n\tdo not believe in vote begging, nor political campaigns, \n\tbut rather in the development of self-thinking individuals."'
 
@@ -8,6 +8,6 @@ message_quote = '\n\t"Anarchists know that a long period of education must \n\tp
 print(famouse_person.title() + message_validation + message_quote)
 print('\n')
 
-# Wish to meet the famous person message print
-print("Hello " + famous_person.title() + ", I wish I could ask you about your opinions.")
+# Wish to ask for opinions
+print("Hello " + famouse_person.title() + ", I wish I could ask you about your opinions.")
 print(my_name.title())
