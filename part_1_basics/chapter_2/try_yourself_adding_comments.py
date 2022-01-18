c1425dcca4b6023043b24ad320e4866e64aba74b
serinko <johanesxsalmon@riseup.net>
2022-01-18 18:32:46 +0100

    edits

--- part_1_basics/chapter_2/try_yourself_adding_comments.py
+++ part_1_basics/chapter_2/try_yourself_adding_comments.py
@@ -1,10 +1,13 @@
-# Practicing variables, spaces and tabs, to create a quote. Name message can be changed.
-name = "lucy parsons" 
+# variables definition
+my_name = "serinko"
+famous_person = "lucy parsons" 
 message_validation = "'s wise words are still very valid. Just think about them:"
 message_quote = '\n\t"Anarchists know that a long period of education must \n\tprecede any great fundamental change in society, hence they \n\tdo not believe in vote begging, nor political campaigns, \n\tbut rather in the development of self-thinking individuals."'
 
-print(name.title() + message_validation + message_quote)
+# Famouse person quote print
+print(famouse_person.title() + message_validation + message_quote)
 print('\n')
 
-# Last variables giving print, wishing to meet the famouse person
-print("Hello " + name.title() + ", I wish I could ask you about your opinions")
+# Wish to meet the famous person message print
+print("Hello " + famous_person.title() + ", I wish I could ask you about your opinions.")
+print(my_name.title())
