14026277bd051844cef2dae14ff3fd609d361d41
serinko <johanesxsalmon@riseup.net>
2022-01-19 10:25:21 +0100

    list accessing elements

--- part_1_basics/chapter_3/bicycles.py
+++ part_1_basics/chapter_3/bicycles.py
@@ -1,7 +1,10 @@
-
+# This is how lists look like, using [] brackets.
 bicycles = ['trek','cannondale', 'redline', 'specialized']
+
+# To print an item, use an index number of the item
 print(bicycles[0])
 
 print("\n")
 
+# Methods learnt in chapter_2 can be used. ie .title() to keep the grammar
 print(bicycles[3]title())
