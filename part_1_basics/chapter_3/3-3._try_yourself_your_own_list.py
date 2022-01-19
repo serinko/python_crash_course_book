0e35a50531a3133fcf5fd45e3d236aae16a243fe
serinko <johanesxsalmon@riseup.net>
2022-01-19 11:32:35 +0100

    3-3, bug detection

--- part_1_basics/chapter_3/3-3._try_yourself_your_own_list.py
+++ part_1_basics/chapter_3/3-3._try_yourself_your_own_list.py
@@ -1,12 +1,15 @@
 # make a list of means of transport and different messages, print them, using the list variables
 
-modes_of_transportation = ['bicycle', 'walk', 'horse', 'plane']
-index_1 = 
-message_1 = "I use " + transportation[index_1] + "as a most prioritized mean of trnsportation.\n"
+modes_of_transportation = ['bicycle', 'walk', 'horse', 'plane', 'car']
+index_1 = -1
+message_1 = "I use a " + modes_of_transportation[index_1] + "as a most prioritized mean of trnsportation.\n"
 
-index_2 =
-message_2 = "I wish to use a " + transportation[index_2] + " more often to move around.\n"
+index_2 = 2
+message_2 = "I wish to use a " + modes_of_transportation[index_2] + " more often to move around.\n"
 
-index_3 = 
-message_3 = "I will try to go by " +transportatin[index_3] + " less than I do.\n"
+index_3 = -2
+message_3 = "I will try to go by " + modes_of_transportation[index_3] + " less than I do.\n"
 
+print(message_1)
+print(message_2)
+print(message_3)
