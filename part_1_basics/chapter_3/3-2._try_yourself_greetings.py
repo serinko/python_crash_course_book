43b16f456e96a82fa7ccf8f046a7587eed5c64f8
serinko <johanesxsalmon@riseup.net>
2022-01-19 14:57:03 +0100

    grammar edit

--- part_1_basics/chapter_3/3-2._try_yourself_greetings.py
+++ part_1_basics/chapter_3/3-2._try_yourself_greetings.py
@@ -6,7 +6,7 @@ names = ['ara', 'helin', 'orso', 'ceren']
 # Changing index, prints the same message with different name. The simpliest way to do it.
 index = 1
 
-# the message to be printed
+# The message to be printed
 message = "Dear " + names[index].title() + ", I think about you often. Thank you for your guidance in the times of doubts. Yours"
 
 print(message)
