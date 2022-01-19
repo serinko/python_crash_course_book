b56c961d4d026bed5d5539c6fe8b14b01e05be4a
serinko <johanesxsalmon@riseup.net>
2022-01-19 10:39:17 +0100

    position -1

--- part_1_basics/chapter_3/bicycles.py
+++ part_1_basics/chapter_3/bicycles.py
@@ -7,4 +7,9 @@ print(bicycles[0])
 print("\n")
 
 # Methods learnt in chapter_2 can be used. ie .title() to keep the grammar
-print(bicycles[3].title())
+print(bicycles[2].title())
+
+print("\n")
+
+# A syntax -1, returns the last item from the list
+print(bicycles[-1])
