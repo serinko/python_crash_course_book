305e56a7db02787c12bcc1bea9eca53aff06ff23
serinko <johanesxsalmon@riseup.net>
2022-01-19 11:13:10 +0100

    3-2, using index to print message

--- part_1_basics/chapter_3/3-2._try_yourself_greetings.py
+++ part_1_basics/chapter_3/3-2._try_yourself_greetings.py
@@ -1,1 +1,14 @@
+# excercise: mae a message and print it personalised to each of the friend.
 
+# list of the names
+names = ['ara', 'helin', 'orso', 'ceren']
+
+# index is a try out to see if I can make a variable for each name, wo not to have to repreint the message)
+index = 0
+
+# the message to be printed
+message = "Dear " + names[index].title() + ", I think about you often. Thank you for your guidance in the times of doubts. Yours"
+print(message)
+print("\n")
+
+# variating the index, I can just use to variables and print the message with a simple function
