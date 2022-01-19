9901c37c6659a8d645b96c5b3c6fc7dd4de14725
serinko <johanesxsalmon@riseup.net>
2022-01-19 10:58:09 +0100

    3-1 printing names

--- part_1_basics/chapter_3/3-1._try_yourself_names.py
+++ part_1_basics/chapter_3/3-1._try_yourself_names.py
@@ -1,1 +1,13 @@
+# this is an excercise to try to make a list of names and print them, using their index
+names = ['ara', 'helin', 'orso', 'ceren']
+print(names[0].title())
+print("\n")
 
+print(names[1].title())
+print("\n")
+
+print(names[2].title())
+print("\n")
+
+print(names[3].title())
+print("\n")
