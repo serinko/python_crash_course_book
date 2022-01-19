9f4890c335b3b9af1924d769ddcf278427de2758
serinko <johanesxsalmon@riseup.net>
2022-01-19 11:18:52 +0100

    3-2, simplyfied, finished

--- part_1_basics/chapter_3/3-2._try_yourself_greetings.py
+++ part_1_basics/chapter_3/3-2._try_yourself_greetings.py
@@ -1,10 +1,10 @@
-# excercise: mae a message and print it personalised to each of the friend.
+# excercise: make a message and print it personalised to each of the friend.
 
 # list of the names
 names = ['ara', 'helin', 'orso', 'ceren']
 
 # Changing index, prints the same message with different name. The simpliest way to do it.
-index = 0
+index = 1
 
 # the message to be printed
 message = "Dear " + names[index].title() + ", I think about you often. Thank you for your guidance in the times of doubts. Yours"
