925a8f685ec22ea12209cece4bb1d8936e9a6d12
serinko <johanesxsalmon@riseup.net>
2022-01-19 10:43:10 +0100

    counting index from the end

--- part_1_basics/chapter_3/bicycles.py
+++ part_1_basics/chapter_3/bicycles.py
@@ -3,13 +3,16 @@ bicycles = ['trek','cannondale', 'redline', 'specialized']
 
 # To print an item, use an index number of the item
 print(bicycles[0])
-
 print("\n")
 
 # Methods learnt in chapter_2 can be used. ie .title() to keep the grammar
 print(bicycles[2].title())
-
 print("\n")
 
 # A syntax -1, returns the last item from the list
 print(bicycles[-1])
+print("\n")
+
+# Any [-x] can be used to return an index counted - from the end.
+print(bicycles[-3])
+print("\n")
