c8777d5a7118beffc8782d7d07fed5a2a407c55a
serinko <johanesxsalmon@riseup.net>
2022-01-19 11:18:15 +0100

    3-2, simplyfied, finished

--- part_1_basics/chapter_3/3-2._try_yourself_greetings.py
+++ part_1_basics/chapter_3/3-2._try_yourself_greetings.py
@@ -3,26 +3,10 @@
 # list of the names
 names = ['ara', 'helin', 'orso', 'ceren']
 
-# index is a try out to see if I can make a variable for each name, wo not to have to repreint the message)
+# Changing index, prints the same message with different name. The simpliest way to do it.
 index = 0
 
 # the message to be printed
 message = "Dear " + names[index].title() + ", I think about you often. Thank you for your guidance in the times of doubts. Yours"
-print(message)
-print("\n")
 
-# variating the index, I can just use to variables and print the message with a simple function
-index = 1
-message = "Dear " + names[index].title() + ", I think about you often. Thank you for your guidance in the times of doubts. Yours" 
 print(message)
-print("\n")
-
-index = 2
-message = "Dear " + names[index].title() + ", I think about you often. Thank you for your guidance in the times of doubts. Yours" 
-print(message)
-print("\n")
-
-index = 3
-message = "Dear " + names[index].title() + ", I think about you often. Thank you for your guidance in the times of doubts. Yours" 
-print(message)
-print("\n")
