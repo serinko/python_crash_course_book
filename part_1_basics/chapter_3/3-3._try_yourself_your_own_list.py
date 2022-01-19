dd04a29b157deffff1fcfec540aa04f8a780f143
serinko <johanesxsalmon@riseup.net>
2022-01-19 11:29:17 +0100

    3-3, transportation, list and messages

--- part_1_basics/chapter_3/3-3._try_yourself_your_own_list.py
+++ part_1_basics/chapter_3/3-3._try_yourself_your_own_list.py
@@ -1,1 +1,12 @@
+# make a list of means of transport and different messages, print them, using the list variables
+
+modes_of_transportation = ['bicycle', 'walk', 'horse', 'plane']
+index_1 = 
+message_1 = "I use " + transportation[index_1] + "as a most prioritized mean of trnsportation.\n"
+
+index_2 =
+message_2 = "I wish to use a " + transportation[index_2] + " more often to move around.\n"
+
+index_3 = 
+message_3 = "I will try to go by " +transportatin[index_3] + " less than I do.\n"
 
