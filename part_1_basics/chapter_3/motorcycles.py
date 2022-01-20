111c51e5e7a525b6db8f85451e8060695daa1461
serinko <johanesxsalmon@riseup.net>
2022-01-20 12:16:40 +0100

    motorcycles created

---
+++ part_1_basics/chapter_3/motorcycles.py
@@ -0,0 +1,7 @@
+motorcycles = ['honda', 'yamaha', 'suzuki',]
+	# List of motorcycles, honda in position 0 is the first element
+print(motorcycles)
+
+motorcycles[0] = 'ducati'
+	# it changes the value of the first item to ducati, the resto of the list remains
+print(motorcycles)
