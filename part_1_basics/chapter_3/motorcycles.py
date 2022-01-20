e715aeb15e7996fabf4c890dfd138544aab3acec
serinko <johanesxsalmon@riseup.net>
2022-01-20 13:26:44 +0100

    .append() function introduced

--- part_1_basics/chapter_3/motorcycles.py
+++ part_1_basics/chapter_3/motorcycles.py
@@ -5,3 +5,7 @@ print(motorcycles)
 motorcycles[0] = 'ducati'
 	# it changes the value of the first item to ducati, the resto of the list remains
 print(motorcycles)
+
+motorcycles.append('hurley')
+	# .append adds 'hurley' to the end of the list without any impact on the order of the list
+print(motorcycles)
