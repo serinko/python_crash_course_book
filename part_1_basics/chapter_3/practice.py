b7228c2a07dd21e7cb9e479b0ff0e9983c484cf6
serinko <johanesxsalmon@riseup.net>
2022-01-24 16:45:55 +0100

    message

--- part_1_basics/chapter_3/practice.py
+++ part_1_basics/chapter_3/practice.py
@@ -1,4 +1,8 @@
-message = "Please write down your favourite colors."
+message_introduction = "Please write down your favourite colors."
 	# This is an intdoctudion message
-print(message)
+print(message_introduction)
 	# This will print it for the reader
+	
+favorite_colors = []
+
+
