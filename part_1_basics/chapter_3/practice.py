64253b324905c2a632657e65b29310be7c930fff
serinko <johanesxsalmon@riseup.net>
2022-01-25 16:09:06 +0100

    fixing bugs

--- part_1_basics/chapter_3/practice.py
+++ part_1_basics/chapter_3/practice.py
@@ -18,6 +18,9 @@ print(favorite_colors[0] + ', ' + favorite_colors[1] + ', ' + favorite_colors[2]
 	# Picked the colors from the list to print them for the user
 	# Be aware of spelling - favourite doesn't = favorite
 
+print("\n")
+	# adds a space
+	
 message_describe_colors = 'Please describe why each of those are your favorite colors in a sentence.\n'
 	# ask to write one sentence to descibe the colors in the list
 	
