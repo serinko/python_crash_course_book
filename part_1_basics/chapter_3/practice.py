379f7c87e3898da497c1cf5dc60bf848840b8835
serinko <johanesxsalmon@riseup.net>
2022-01-26 13:51:45 +0100

    playing with variables

--- part_1_basics/chapter_3/practice.py
+++ part_1_basics/chapter_3/practice.py
@@ -11,7 +11,7 @@ print(message_2)
 	# Marks the input by the user
 favorite_colors.append('black')
 favorite_colors.append('green')
-favorite_colors.append('turcois')
+favorite_colors.append('purple')
 	# favorite_colors added by user with a function to add to the list
 
 print(favorite_colors[0] + ', ' + favorite_colors[1] + ', ' + favorite_colors[2])
