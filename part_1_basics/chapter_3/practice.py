fcd437023547fb2cb99404e6d5d097afd92eef86
serinko <johanesxsalmon@riseup.net>
2022-01-27 17:03:20 +0100

    adding index

--- part_1_basics/chapter_3/practice.py
+++ part_1_basics/chapter_3/practice.py
@@ -24,16 +24,16 @@ print(favorite_colors[0] + ', ' + favorite_colors[1] + ', ' + favorite_colors[2]
 print("\n")
 	# adds a space
 	
-index_1 = 0
-		# To change index instead of a variable in each sentence
-	
 message_describe_colors = 'Please describe why each of those are your favorite colors in a sentence.\n'
 	# ask to write one sentence to descibe the colors in the list
 	
 print(message_describe_colors)
 	# print the message
+
+index_1 = 0
+	# To change index instead of a variable in each sentence
 	
-message_1 = 'When it comes to my style, I would chose ' + favorite_colors[0] + ' as my number one to wear.\n'
+message_1 = 'When it comes to my style, I would chose ' + favorite_colors[index_1] + ' as my number one to wear.\n'
 	# Makes a color chosen from the list to be chosen for clothes
 
 message_2 = favorite_colors[1].title() + ' is my favorite one, when it comes to every day surounding.\n'
