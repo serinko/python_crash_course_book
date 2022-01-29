cc9311e141b94106e97a5d492ac04d2d251c2bc6
serinko <johanesxsalmon@riseup.net>
2022-01-29 14:35:34 +0100

    edits

--- part_1_basics/chapter_3/practice.py
+++ part_1_basics/chapter_3/practice.py
@@ -12,7 +12,7 @@ print(message_2)
 	# Marks the input by the user
 favorite_colors.append('"grey")
 favorite_colors.append("green")
-favorite_colors.append('purple")
+favorite_colors.append("purple")
 	# favorite_colors added by user with a function to add to the list
 
 
