3451a16f9cd26265b17f8f47f1446cd6d7882117
serinko <johanesxsalmon@riseup.net>
2022-02-01 11:30:12 +0100

    changing_gust_list, removing odin, adding diana

--- part_1_basics/chapter_3/3-5._try_yourself_changing_guest_list.py
+++ part_1_basics/chapter_3/3-5._try_yourself_changing_guest_list.py
@@ -26,3 +26,23 @@ guest_index = guest_list[3]
 message_invitation = "\nDear " + guest_index.title() + " I would like to invite you for a dinner at my place, tonight. Hope to see you there."
 print(message_invitation)
 
+print("\n")
+
+	# ODIN CANNOT COME!
+	# print that statement
+	# Invite an extra person
+	# Change the list accodingly
+	# Invite everyone
+	
+cannot_come = guest_list.pop(1)
+	# Defyning that index 1 = odin, cannot come as a variable
+	# using pop(function)
+message_cannot_come = "Dear guests, I would like to inform you, that " + cannot_come.title() + " will not be able to join us today."
+	# message is defined including the variable cannt_come
+print(message_cannot_come)
+	# printing message to the guest's informing that one cannot_come
+	
+guest_list.append ('diana')
+	 # adds a new guest diana in the end of the guest list
+	 
+	 
