7c16a67174d3f23704191660ec6fbe4df3b60d98
serinko <johanesxsalmon@riseup.net>
2022-02-01 11:05:24 +0100

    changing_gust_list.py starting with the list

--- part_1_basics/chapter_3/3-5._try_yourself_changing_guest_list.py
+++ part_1_basics/chapter_3/3-5._try_yourself_changing_guest_list.py
@@ -1,1 +1,28 @@
 
+guest_list = ['vesna', 'odin', 'hecate', 'maria']
+	# Defyning a list of invited people
+guest_index = guest_list[0]
+	# Defyning the guet from the list according their index
+	# Making a variable esy to eaxchange further on
+message_invitation = "Dear " + guest_index.title() + " I would like to invite you for a dinner at my place, tonight. Hope to see you there."
+	# invitation message as a variable with the index variable
+	# Adding title() method for a correct grammar of the name.
+print(message_invitation)
+	# printing the invitation message, index 0 + title() = Vesna
+	
+	# REPEAT FOR OTHER GUESTS
+
+guest_index = guest_list[1]
+message_invitation = "\nDear " + guest_index.title() + " I would like to invite you for a dinner at my place, tonight. Hope to see you there."
+print(message_invitation)
+
+
+guest_index = guest_list[2]
+message_invitation = "\nDear " + guest_index.title() + " I would like to invite you for a dinner at my place, tonight. Hope to see you there."
+print(message_invitation)
+
+
+guest_index = guest_list[3]
+message_invitation = "\nDear " + guest_index.title() + " I would like to invite you for a dinner at my place, tonight. Hope to see you there."
+print(message_invitation)
+
