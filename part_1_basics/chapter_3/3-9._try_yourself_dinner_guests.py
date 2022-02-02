b6287e7ad599ba36585d5ccee46bd2ca31ddb5c6
serinko <johanesxsalmon@riseup.net>
2022-02-02 10:28:09 +0100

    excercise done

--- part_1_basics/chapter_3/3-9._try_yourself_dinner_guests.py
+++ part_1_basics/chapter_3/3-9._try_yourself_dinner_guests.py
@@ -12,8 +12,9 @@ guest_list.append('nikola')
 print(guest_list)
 	# To check the current state of the list
 	
-message_number_of_guests = "There is " + str(len(guest_list)) + " of guests invited to tonight's dinner."
+message_number_of_guests = "There is " + str(len(guest_list)) + " guests invited to tonight's dinner."
 	# Defines variable with len() function and quotes
 	# len() function  counts the ammount in the list
+	# str() is very important to tell Python that the number is a string, not an index
 	
 print(message_number_of_guests)
