38f9941a1a47ffdd14ac07f9d984c26543d3d0b4
serinko <johanesxsalmon@riseup.net>
2022-02-02 10:06:40 +0100

    locations added

--- part_1_basics/chapter_3/3-8._try_yourself_seeing_the_world.py
+++ part_1_basics/chapter_3/3-8._try_yourself_seeing_the_world.py
@@ -1,1 +1,21 @@
+	# EXCERCISE POINTS
+	# Think of five places you want to see
+	# Store them in a list not sorted
+	# Print it raw as it is
+	# use sorted() to print it without modifying the order
+	# Show that your list is still in the original order
+	# use sorted() to print it in an alphabetically reversed order
+	# show that the list is still in an original order
+	# use reverse() to chane the order of your list
+	# use the reverse() agao and show it is in original order again
+	# use sort() to sort the list in alphabetical order permanently
+	# use sort() to sort your list in an alphabeticaly reversed order
+
+
+locations_to_see = ['chiapas', 'bajkal', 'sarek', 'sumava', 'bakur']
+print("These are some of the locations I would like to visit")
+print(locations_to_see)
+print("\n"
+
+
 
