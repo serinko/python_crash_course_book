8a73203845dacb38abdbb81f7a8ce7e79e9d5a3e
serinko <johanesxsalmon@riseup.net>
2022-02-02 09:08:30 +0100

    cars.py reverse=True

--- part_1_basics/chapter_3/cars.py
+++ part_1_basics/chapter_3/cars.py
@@ -3,3 +3,10 @@ cars.sort()
 	# Method sort() made the list sorted alphabetically
 	# This change is PREMANENT! 
 print(cars)
+
+print("\n")
+
+cars.sort(reverse=True)
+	# Reverse the order backward
+	# Again stays permanent
+print(cars)
