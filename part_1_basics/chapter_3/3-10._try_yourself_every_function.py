740ec47985904da07574f7a44c0436982fba050b
serinko <johanesxsalmon@riseup.net>
2022-02-02 10:42:53 +0100

    adding functions

--- part_1_basics/chapter_3/3-10._try_yourself_every_function.py
+++ part_1_basics/chapter_3/3-10._try_yourself_every_function.py
@@ -1,1 +1,24 @@
+	# EXCERCISE POINTS
+	# Make a list of your choice
+	# Use every function introduced in this chaper
+	
+trees = ['pine', 'oak', 'ash', 'willow', 'beach', 'birch', 'fir']
+trees.insert(3, 'almond')
+trees.append ('hawthorn')
+print(trees)
+
+print(sorted(trees))
+print(trees)
+
+del trees[2]
+print(trees)
+
+trees[-1] = 'rowan'
+print(trees)
+
+too_exotic = 'almond'
+trees.remove(too_exotic)
+print(trees)
+print(sorted(trees, reverse=True))
+print(trees)
 
