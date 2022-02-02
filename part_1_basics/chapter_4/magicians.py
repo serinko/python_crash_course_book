4c803d5a0254d974d0ffa8ccf412a684b98e9975
serinko <johanesxsalmon@riseup.net>
2022-02-02 13:33:47 +0100

    magicians - for loop function

--- part_1_basics/chapter_4/magicians.py
+++ part_1_basics/chapter_4/magicians.py
@@ -1,3 +1,5 @@
 magicians = ['alice','david','carolina']
 for magicians in magicians:
 	print(magicians.title())
+	# for is a loop function
+	# firt time I can see that I used a function print in moved under another function 
