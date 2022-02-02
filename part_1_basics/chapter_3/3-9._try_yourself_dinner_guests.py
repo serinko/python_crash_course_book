03ffcc935b981bd97d0812572f7483769a537bcb
serinko <johanesxsalmon@riseup.net>
2022-02-02 10:27:18 +0100

    first commit

---
+++ part_1_basics/chapter_3/3-9._try_yourself_dinner_guests.py
@@ -0,0 +1,19 @@
+
+guest_list = ['vesna', 'odin', 'hecate', 'maria']
+	# Defyning a list of invited people
+	
+	
+guest_list.insert(0, 'moses')
+guest_list.insert(2, 'andrei')	
+	# Added gues moses on index 0 and andrei on index 2
+	
+guest_list.append('nikola')
+	# Added nikola to the end of the list
+print(guest_list)
+	# To check the current state of the list
+	
+message_number_of_guests = "There is " + str(len(guest_list)) + " of guests invited to tonight's dinner."
+	# Defines variable with len() function and quotes
+	# len() function  counts the ammount in the list
+	
+print(message_number_of_guests)
