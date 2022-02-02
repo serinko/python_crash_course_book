df4961a662ff0703190d746b88a3055606c0cb25
serinko <johanesxsalmon@riseup.net>
2022-02-02 11:14:10 +0100

    excercise done

--- part_1_basics/chapter_3/3-10._try_yourself_every_function.py
+++ part_1_basics/chapter_3/3-10._try_yourself_every_function.py
@@ -22,3 +22,39 @@ print(trees)
 print(sorted(trees, reverse=True))
 print(trees)
 
+trees.sort()
+print(trees)
+
+trees.remove('rowan')
+print(trees)
+
+not_around = trees.pop(1)
+message_not_around = "There is no " + not_around.title() + " around here."
+print(message_not_around)
+
+print(trees)
+trees.reverse()
+print(trees)
+
+trees.append('apple')
+print(trees)
+trees.sort()
+print(trees)
+
+print("\n")
+message_trees_count = "There is " + str(len(trees)) + " trees in the list at the moment."
+
+message_list_state = "\n\n" + message_trees_count + "\nThe current and aphabetically sorted version of the list looks like this:\n" + str(sorted(trees)) + ". \nWhere the " + str(trees[0]).title() + " tree is the first one, and the " + str(trees[-1]).title() + " tree is the last one in the list."
+print(message_list_state)
+
+trees.append('birch')
+trees.remove('apple')
+trees[0] = 'maple'
+trees.insert(3, 'cypres')
+trees.sort()
+
+message_trees_count = "There is " + str(len(trees)) + " trees in the list at the moment."
+message_list_state = "\n\n" + message_trees_count + "\nThe current and aphabetically sorted version of the list looks like this:\n" + str(sorted(trees)) + ". \nWhere the " + str(trees[0]).title() + " tree is the first one, and the " + str(trees[-1]).title() + " tree is the last one in the list."
+print(message_list_state)
+
+
