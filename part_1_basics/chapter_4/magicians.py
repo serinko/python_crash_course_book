9696d3e1d0b581779de62967e8301f5e27d1da97
serinko <johanesxsalmon@riseup.net>
2022-02-02 13:54:41 +0100

    magicians - for loop function

--- part_1_basics/chapter_4/magicians.py
+++ part_1_basics/chapter_4/magicians.py
@@ -1,5 +1,11 @@
 magicians = ['alice','david','carolina']
 for magician in magicians:
-	print(magician.title())
+	print(magician)
 	# for is a loop function
 	# firt time I can see that I used a function print in moved under another function 
+	# for ITEM in LIST_OF_ITEMS
+	# singular and plural helps to navigate if we talk about the list or variable
+	print("\n")
+
+for magician in sorted(magicians):
+	print(magician.title())
