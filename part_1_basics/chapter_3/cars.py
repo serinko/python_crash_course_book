8120bc974547fe2754dd306c0b7a476bf08bd03e
serinko <johanesxsalmon@riseup.net>
2022-02-02 09:34:54 +0100

    cars.py sorted reverse

--- part_1_basics/chapter_3/cars.py
+++ part_1_basics/chapter_3/cars.py
@@ -10,3 +10,24 @@ cars.sort(reverse=True)
 	# Reverse the order backward
 	# Again stays permanent
 print(cars)
+
+print("\n")
+
+cars = ['bmw','audi','toyota','subaru']
+print("Here is the original list:")
+print(cars)
+
+print("\nHere is the sorted list:")
+print(sorted(cars))
+	# Adding sorted() where cars are in ()
+	# This displays the list sorted alphabethically
+	
+print("\n")
+
+print("\nHere is the original list again:")
+print(cars)
+	# Without changng anything on the list, it prints as the original
+
+print(sorted(cars, reverse=True))
+	# This displays reverse order
+	# It is temporarily - the list still stays
