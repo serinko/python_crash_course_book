6264f6d2b182311212ba89f9f76fe4ba5cfaa9c5
serinko <johanesxsalmon@riseup.net>
2022-02-03 13:48:32 +0100

    making number ranges with

--- part_1_basics/chapter_4/even_numbers.py
+++ part_1_basics/chapter_4/even_numbers.py
@@ -1,1 +1,14 @@
+even_numbers = list(range(2,11,2))
+	# (2 = start of the range, 11 = end of range, 2 = adding ammount)
+print(even_numbers)
+print("\n")
 
+odd_numbers = list(range(1,11,2))
+	# (1 = start of the range, 11 = end of range, 2 = adding ammount)
+print(odd_numbers)
+print("\n")
+
+denominator_3 = list(range(3,31,3))
+	# sme logic
+print(denominator_3)
+print("\n")
