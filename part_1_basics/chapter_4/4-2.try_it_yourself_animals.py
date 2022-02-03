c01cc873bf5e8b2db99c80a750b6b8ea0f5165a8
serinko <johanesxsalmon@riseup.net>
2022-02-03 11:31:18 +0100

    animals excercise - done

--- part_1_basics/chapter_4/4-2.try_it_yourself_animals.py
+++ part_1_basics/chapter_4/4-2.try_it_yourself_animals.py
@@ -1,1 +1,11 @@
-
+water_animals = ['shark', 'turtle', 'octopus', 'shrimp']
+for animal in water_animals:
+	message_1 = animal.title() + " is a water creature.\n"
+	# variable to print with each animal
+	print(message_1)
+	# print function for each animal in water_animals
+	
+message_2 = "Water animals can look very different from each other, still they are all adapted to the life under water."
+	# message is not in a loop, its for all the animals
+print(message_2)
+	# print the message in the end of the program
