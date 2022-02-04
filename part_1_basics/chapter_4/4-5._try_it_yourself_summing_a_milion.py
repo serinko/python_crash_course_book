250b4301fff69cfbc66cec58add69476329a5fdc
serinko <johanesxsalmon@riseup.net>
2022-02-04 11:42:12 +0100

    excercise done

--- part_1_basics/chapter_4/4-5._try_it_yourself_summing_a_milion.py
+++ part_1_basics/chapter_4/4-5._try_it_yourself_summing_a_milion.py
@@ -1,1 +1,16 @@
+	# EXCERCISE:
+	# make a list 1-1000000
+	# print min
+	# print max
+	# make sure min is 1 and max 1 milion exactly
+	# sum it up and watch how quickly python calculates
+	
+milion = list(range(1,1000001))
+print(min(milion))
+print("\n")
+
+print(max(milion))
+print("\n")
+
+print(sum(milion))
 
