3744d2db0374b93194cdd2a1c39aa1f4805542a6
serinko <johanesxsalmon@riseup.net>
2022-02-04 12:52:54 +0100

    excercise done

--- part_1_basics/chapter_4/4-8._try_it_yourself_cubes.py
+++ part_1_basics/chapter_4/4-8._try_it_yourself_cubes.py
@@ -1,1 +1,14 @@
-
+	# EXCERCISE:
+	# A number raised to the third power is calle dcube
+	# ie the cube of 2 is expressed as 2**3
+	# Make the list of the frst cubes in (cube for each integer 1-10)
+	# use for loop to print value of each
+	
+cubes = [value**3 for value in range(1,11)]
+	# defining the list cubes
+	# using the variable value**3 loping for each step in the range
+	
+for cubes in cubes:
+	# for loop with a variable cube and list of cubes
+	print(cubes)
+	# finally printing
