14fbc28511995654c72ff714d9129e237e459d8f
serinko <johanesxsalmon@riseup.net>
2022-02-04 10:44:30 +0100

    digits and functions

--- part_1_basics/chapter_4/simple_statistics.py
+++ part_1_basics/chapter_4/simple_statistics.py
@@ -1,8 +1,16 @@
 digits = list(range(0,41,2))
+	# Creates list of digits in range 0,41, by 2
 print (digits)
+	#control print of the list
 print("\n")
+
 print(min(digits))
+	# "min" function shows the smallest digit from "digits"
 print("\n")
+
 print(max(digits))
+	# "max" function shows the biggest digit from "digits"
 print("\n")
+
 print(sum(digits))
+	# "sum" function shows the sum value "digits"
