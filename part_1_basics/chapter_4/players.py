4de0d9865a2c1c37a217a8377231b15f06bcbb05
serinko <johanesxsalmon@riseup.net>
2022-02-05 09:40:50 +0100

    slice method

--- part_1_basics/chapter_4/players.py
+++ part_1_basics/chapter_4/players.py
@@ -7,3 +7,4 @@ print(players[0:3])
 	# as in range the frs tindex is where the range starts
 	# the last marks the border, so the slice prints -1 from it
 	# because list counts from 0, do nt forget off-by-one rule
+print(players[1:4])
