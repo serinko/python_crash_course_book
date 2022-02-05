ec43f5aa48a4741195030757a5c251ad895ac8cd
serinko <johanesxsalmon@riseup.net>
2022-02-05 09:16:34 +0100

    slice method

--- part_1_basics/chapter_4/players.py
+++ part_1_basics/chapter_4/players.py
@@ -1,1 +1,9 @@
-
+players = ['charles', 'martina', 'michael', 'florence', 'eli']
+	# defnes a list of "players"
+print(players[0:3])
+	# Same like when defining an item, we used "slice"
+	# this defines a group of items from the list
+	# when range is (,) slice uses [:]
+	# as in range the frs tindex is where the range starts
+	# the last marks the border, so the slice prints -1 from it
+	# because list counts from 0, do nt forget off-by-one rule
