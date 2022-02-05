c22feb24c27aba48b3b6a3b39dca55a82cf1eb3d
serinko <johanesxsalmon@riseup.net>
2022-02-05 10:21:17 +0100

    first commit

---
+++ part_1_basics/chapter_4/foods.py
@@ -0,0 +1,9 @@
+my_foods = ['pizza', 'falafel', 'carrot cake']
+friends_food = my_foods[:]
+
+print("My favorite foods are:")
+print(my_foods)
+
+print("\n My friends favorite foods are:")
+print(friends_food)
+
