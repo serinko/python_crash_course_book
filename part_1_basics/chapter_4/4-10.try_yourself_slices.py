9af2ac98de6c2a4443abacfb8ab285d22049c54f
serinko <johanesxsalmon@riseup.net>
2022-02-05 10:49:48 +0100

    excercise done

--- part_1_basics/chapter_4/4-10.try_yourself_slices.py
+++ part_1_basics/chapter_4/4-10.try_yourself_slices.py
@@ -13,3 +13,14 @@ print("\n")
 	# We can also print them not in the list form but as items
 	
 
+print("These are the first three players from the list:")
+for player in players[:3]:
+	print(player.title())
+	
+print("\nThese are the three players from the middle of the list:")
+for player in players[1:-1]:
+	print(player.title())
+
+print("\nThese are the last three players from the list:")
+for player in players[-3:]:
+	print(player.title())
