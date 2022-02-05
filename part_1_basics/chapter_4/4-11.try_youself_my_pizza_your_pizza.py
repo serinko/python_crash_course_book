85ff84ff353801fff72f9cafa1086bc1e289e5d8
serinko <johanesxsalmon@riseup.net>
2022-02-05 11:03:30 +0100

    excercise done

--- part_1_basics/chapter_4/4-11.try_youself_my_pizza_your_pizza.py
+++ part_1_basics/chapter_4/4-11.try_youself_my_pizza_your_pizza.py
@@ -1,1 +1,15 @@
+my_pizzas = ['margarita', 'capriciosa', 'quattro formaggi']
+friends_pizzas = my_pizzas[:]
 
+my_pizzas.append('frutti di mare')
+friends_pizzas.append('fungi')
+
+print("My favorite pizas are:")
+for pizza in my_pizzas:
+	print(pizza.title())
+	
+print("\n")
+
+print("My friend's favorite pizas are:")
+for pizza in friends_pizzas:
+	print(pizza.title())
