2c9aafb018982cc3d65d1823052afc62b4a2abaf
serinko <johanesxsalmon@riseup.net>
2022-02-05 11:11:00 +0100

    excercise done

--- part_1_basics/chapter_4/4-12.try_yourself_more_loops.py
+++ part_1_basics/chapter_4/4-12.try_yourself_more_loops.py
@@ -3,19 +3,19 @@ friends_food = my_foods[:]
 
 print("My favorite foods are:")
 for food in my_foods:
-	print(food)
+	print(food.title())
 
 print("\nMy friends favorite foods are:")
 for food in friends_food:
-	print(food)
+	print(food.title())
 
 my_foods.append('cannoli')
 friends_food.append('ice cream')
 
 print("\nMy favorite foods are:")
 for food in my_foods:
-	print(food)
+	print(food.title())
 	
 print("\nMy friends favorite foods are:")
 for food in friends_food:
-	print(food)
+	print(food.title())
