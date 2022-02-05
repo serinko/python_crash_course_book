cfda0851865277228b8077c4841c9ce9c9ebfbe0
serinko <johanesxsalmon@riseup.net>
2022-02-05 10:47:35 +0100

    using slices for printing

--- part_1_basics/chapter_4/4-10.try_yourself_slices.py
+++ part_1_basics/chapter_4/4-10.try_yourself_slices.py
@@ -1,1 +1,10 @@
+players = ['charles', 'martina', 'michael', 'florence', 'eli']
+print("These are the first three players from the list:")
+print(players[:3])
+
+print("\nThese are the three players from the middle of the list:")
+print(players[1:-1])
+
+print("\nThese are the last three players from the list:")
+print(players[-3:])
 
