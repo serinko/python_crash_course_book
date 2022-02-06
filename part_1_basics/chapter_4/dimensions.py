2169cb29b32a8cac37392e7ddd4f6e37aa4d9ec3
serinko <johanesxsalmon@riseup.net>
2022-02-06 09:47:01 +0100

    tuple errors

--- part_1_basics/chapter_4/dimensions.py
+++ part_1_basics/chapter_4/dimensions.py
@@ -6,3 +6,9 @@ print(dimensions[0])
 print(dimensions[1])
 	# just printing it's items like in the list
 	
+dimensions[0] = 250
+	# This is not working because tuple cannot be altered
+	# The values defined in dimensions must stay the same
+	# unles the whole tuple is not re-defined
+print(dimensions[0])
+	# we get an error message
