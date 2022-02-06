5e715b645da673851160cb5b19c5c85cbdd689c2
serinko <johanesxsalmon@riseup.net>
2022-02-06 10:12:37 +0100

    tuple loop

--- part_1_basics/chapter_4/dimensions_2.py
+++ part_1_basics/chapter_4/dimensions_2.py
@@ -1,1 +1,14 @@
+dimensions = (200,50)
+	# TUPLE is a list of immutable items
+	# That means that they cannt be changed
+	# defined as a list with () instead of []
+print("Here are the dimesions:\n")
 
+	# you can loop through values in a tuple
+for dimension in dimensions:
+	print(dimension)
+
+dimensions = (220,75)
+print("\nHere are the re-defined dimensions:")
+for dimension in dimensions:
+	print(dimension)
