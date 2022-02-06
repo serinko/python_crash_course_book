28c55765ec4457e8a6d8562c07f1348950effb7e
serinko <johanesxsalmon@riseup.net>
2022-02-06 17:12:45 +0100

    code style

--- part_1_basics/chapter_3/3-7._try_yourself_shrinking_guest_list.py
+++ part_1_basics/chapter_3/3-7._try_yourself_shrinking_guest_list.py
@@ -122,12 +122,17 @@ print(message_invitation)
 	# print a new invitation message to everyone in the list
 	
 
-message_more_seats = "\nDear guests, I found a bigger table, so more people will be invited to today's dinner. The invitations will be printed for everyone in few minutes."
+message_more_seats = \
+	"\nDear guests, I found a bigger table, so more people will be invited to \
+	today's dinner. The invitations will be printed for \
+	everyone in few minutes."
 	# Defining variable with the message about more seats
+
 print(message_more_seats)
 	# Printing that message
 	
 print("\n")
+
 	
 guest_list.insert(0, 'moses')
 guest_list.insert(2, 'andrei')	
@@ -135,40 +140,80 @@ guest_list.insert(2, 'andrei')
 	
 guest_list.append('nikola')
 	# Added nikola to the end of the list
+
 print(guest_list)
 	# To check the current state of the list
 	
+
 	# PRINT INVITATION FOR EVERY CURRENT GUEST!
+
 	
 guest_index = guest_list[0]
-message_invitation = "\nDear " + guest_index.title() + ", I would like to invite you for a dinner at my place, tonight. Hope to see you there."
+
+message_invitation = \
+	"\nDear " + guest_index.title() + \
+	", I would like to invite you for a dinner at my place, tonight. \
+	Hope to see you there."
+
 print(message_invitation)
 
+
 guest_index = guest_list[1]
-message_invitation = "\nDear " + guest_index.title() + ", I would like to invite you for a dinner at my place, tonight. Hope to see you there."
+
+message_invitation = \
+	"\nDear " + guest_index.title() + \
+	", I would like to invite you for a dinner at my place, tonight. \
+	Hope to see you there."
+
 print(message_invitation)
 
 
 guest_index = guest_list[2]
-message_invitation = "\nDear " + guest_index.title() + ", I would like to invite you for a dinner at my place, tonight. Hope to see you there."
+
+message_invitation = \
+	"\nDear " + guest_index.title() + \
+	", I would like to invite you for a dinner at my place, tonight. \
+	Hope to see you there."
+
 print(message_invitation)
 
 
 guest_index = guest_list[3]
-message_invitation = "\nDear " + guest_index.title() + ", I would like to invite you for a dinner at my place, tonight. Hope to see you there."
+
+message_invitation = \
+	"\nDear " + guest_index.title() + \
+	", I would like to invite you for a dinner at my place, tonight. \
+	Hope to see you there."
+
 print(message_invitation)
 
+
 guest_index = guest_list[4]
-message_invitation = "\nDear " + guest_index.title() + ", I would like to invite you for a dinner at my place, tonight. Hope to see you there."
+
+message_invitation = \
+	"\nDear " + guest_index.title() + \
+	", I would like to invite you for a dinner at my place, tonight. \
+	Hope to see you there."
+
 print(message_invitation)
 
 guest_index = guest_list[5]
-message_invitation = "\nDear " + guest_index.title() + ", I would like to invite you for a dinner at my place, tonight. Hope to see you there."
+
+message_invitation = \
+	"\nDear " + guest_index.title() + \
+	", I would like to invite you for a dinner at my place, tonight. \
+	Hope to see you there."
+
 print(message_invitation)
 
 
 guest_index = guest_list[6]
-message_invitation = "\nDear " + guest_index.title() + ", I would like to invite you for a dinner at my place, tonight. Hope to see you there."
+
+message_invitation = \
+	"\nDear " + guest_index.title() + \
+	", I would like to invite you for a dinner at my place, tonight. \
+	Hope to see you there."
+
 print(message_invitation)
 
 print("\n")
@@ -183,8 +228,13 @@ print("\n")
 	# When done, use del to delete the remaining names and empty the list
 	# print the list to make sure it's empty
 	
-message_table_delayed = "\nDear guests, I have a bad news. Just got informed that the table delivery is delayed and therefore I only have capacity for two guests, I am very sorry for this."
+	
+message_table_delayed = 
+	"\nDear guests, I have a bad news. Just got informed that the table \
+	delivery is delayed and therefore I only have capacity for two guests, \
+	I am very sorry for this."
 	# Defining variable with the message about delayed table
+
 print(message_table_delayed)
 	# Printing that message for the guests
 	
@@ -194,48 +244,82 @@ print(message_table_delayed)
 cannot_come = guest_list.pop(0)
 	# Defyning that index 0 is no longer invited
 	# using pop(function)
-message_cannot_come = "\nDear " + cannot_come.title() + ", I am very sorry to inform you, that due to the problems with the delayed table, I must cancell your invitation for today's dinner. I hope to make up for this inconvenience very soon."
+
+message_cannot_come = \
+	"\nDear " + cannot_come.title() + \
+	", I am very sorry to inform you, that due to the problems with the \
+	delayed table, I must cancell your invitation for today's dinner. \
+	I hope to make up for this inconvenience very soon."
 	# message is defined including the variable cannt_come
+
 print(message_cannot_come)
 	# printing message to the guest's informing that one cannot_come
+
 
 cannot_come = guest_list.pop(1)
 	# Defyning that index 1 cannot come and save t as a variable
 	# (2 when 0 was deleted becomes 1)
 	# using pop(function)
-message_cannot_come = "\nDear " + cannot_come.title() + ", I am very sorry to inform you, that due to the problems with the delayed table, I must cancell your invitation for today's dinner. I hope to make up for this inconvenience very soon."
+
+message_cannot_come = \
+	"\nDear " + cannot_come.title() + \
+	", I am very sorry to inform you, that due to the problems with the \
+	delayed table, I must cancell your invitation for today's dinner. \
+	I hope to make up for this inconvenience very soon."
 	# message is defined including the variable cannt_come
+	
 print(message_cannot_come)
 	# printing message to the guest's informing that one cannot_come
+
 
 cannot_come = guest_list.pop()
 	# Defyning variable when the last guest is deleted/ not invited
 	# pop with empty () takes off the last item
 	# using pop(function)
-message_cannot_come = "\nDear " + cannot_come.title() + ", I am very sorry to inform you, that due to the problems with the delayed table, I must cancell your invitation for today's dinner. I hope to make up for this inconvenience very soon."
+
+message_cannot_come = \
+	"\nDear " + cannot_come.title() + \
+	", I am very sorry to inform you, that due to the problems with the \
+	delayed table, I must cancell your invitation for today's dinner. \
+	I hope to make up for this inconvenience very soon."
 	# message is defined including the variable cannt_come
+
 print(message_cannot_come)
 	# printing message to the guest's informing that one cannot_come
+
 	
 cannot_come = guest_list.pop()
 	# Defyning variable when the last guest is deleted/ not invited
 	# pop with empty () takes off the last item
 	# using pop(function)
-message_cannot_come = "\nDear " + cannot_come.title() + ", I am very sorry to inform you, that due to the problems with the delayed table, I must cancell your invitation for today's dinner. I hope to make up for this inconvenience very soon."
+message_cannot_come = \
+	"\nDear " + cannot_come.title() + \
+	", I am very sorry to inform you, that due to the problems with the \
+	delayed table, I must cancell your invitation for today's dinner. \
+	I hope to make up for this inconvenience very soon."
 	# message is defined including the variable cannt_come
+
 print(message_cannot_come)
 	# printing message to the guest's informing that one cannot_come
+
 	
 cannot_come = guest_list.pop()
 	# Defyning variable when the last guest is deleted/ not invited
 	# pop with empty () takes off the last item
 	# using pop(function)
-message_cannot_come = "\nDear " + cannot_come.title() + ", I am very sorry to inform you, that due to the problems with the delayed table, I must cancell your invitation for today's dinner. I hope to make up for this inconvenience very soon."
+
+message_cannot_come = \
+	"\nDear " + cannot_come.title() + \
+	", I am very sorry to inform you, that due to the problems with the \
+	delayed table, I must cancell your invitation for today's dinner. \
+	I hope to make up for this inconvenience very soon."
 	# message is defined including the variable cannt_come
+
 print(message_cannot_come)
 	# printing message to the guest's informing that one cannot_come
-	
+
 print("\n")	
+
 	
 print(guest_list)
 	# print for myself to control the state of the list
@@ -243,20 +327,34 @@ print(guest_list)
 	# Everything is correct, only two guest remained
 	# Invite them, by printing an invitation for each of them
 
+
 guest_index = guest_list[0]
-message_invitation = "\nDear " + guest_index.title() + ", despite the changes, I still would like to invite you for a dinner at my place, tonight. Hope to see you there."
+
+message_invitation = \
+	"\nDear " + guest_index.title() + \
+	", despite the changes, I still would like to invite you for a dinner at \
+	my place, tonight. Hope to see you there."
+
 print(message_invitation)
 
+
 guest_index = guest_list[1]
-message_invitation = "\nDear " + guest_index.title() + ", despite the changes, I still would like to invite you for a dinner at my place, tonight. Hope to see you there."
+
+message_invitation = \
+	"\nDear " + guest_index.title() + \
+	", despite the changes, I still would like to invite you for a dinner \
+	at my place, tonight. Hope to see you there."
+
 print(message_invitation)
 
 print("\n")
 	
 	# Delete everyone fromt he list to clean up
 
+
 del guest_list[0]	
 	# deletes the first indexed guest of the list
+
 del guest_list[0]
 	# since only one guest/item was left in the list, its again the 1st [0]
 
@@ -264,6 +362,3 @@ print(guest_list)
 	# print myself the list to control that it is empty
 	
 	# EXCERCISE DONE!
-
-
-	
