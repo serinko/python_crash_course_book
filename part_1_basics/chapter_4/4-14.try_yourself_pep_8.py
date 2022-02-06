406376e1da71354a5e5d16612c1b8feaa991a4b3
serinko <johanesxsalmon@riseup.net>
2022-02-06 16:37:05 +0100

    excercise done

--- part_1_basics/chapter_4/4-14.try_yourself_pep_8.py
+++ part_1_basics/chapter_4/4-14.try_yourself_pep_8.py
@@ -2,14 +2,25 @@ my_list = [
   1, 2, 3,
   4, 5, 6,
 ]
-    # Ways how to write in pep8 standarts
+
+
+my_list = [
+  1, 2, 3,
+  4, 5, 6,
+  ]
+
+# Ways how to write in pep8 standarts
+
 print(my_list)
 
 
 print("\n")
-# ~ ctrl+E makes this type of comment, which is god to use for code archive
-# ~ Python will ignore this code
+
 
 for number in my_list:
     print(number)
     
+    
+# ~ ctrl+E makes this type of comment, which is god to use for code archive
+# ~ Python will ignore this code
+
