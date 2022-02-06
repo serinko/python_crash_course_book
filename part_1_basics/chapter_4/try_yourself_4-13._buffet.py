2667ce0dcbbd7e5496a54288a09533fc1d213f5d
serinko <johanesxsalmon@riseup.net>
2022-02-06 10:18:44 +0100

    first commit

---
+++ part_1_basics/chapter_4/try_yourself_4-13._buffet.py
@@ -0,0 +1,16 @@
+	# EXCERCISE:
+	# A buffet offer only 5 very simple dishes
+	# make such list and store them in  tuple
+	# use a for loop to print each dish the restaurant offers
+	# Try to modify one and see what Python does
+	# Two dishes change - write a block of code to redefine the tuple
+	# print it through for loop function
+	
+buffet_menu = ('hummus', 'cheese spread', 'guacamole', \
+'pita bread', 'nachos')
+
+print("The buffet menu:")
+for food in buffet_menu:
+	print(food.title())
+	
+
