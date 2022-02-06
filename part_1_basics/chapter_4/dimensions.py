d120aebc3f79627fec011ada42d5c8994dee3c2f
serinko <johanesxsalmon@riseup.net>
2022-02-06 09:41:43 +0100

    introducing tuple

--- part_1_basics/chapter_4/dimensions.py
+++ part_1_basics/chapter_4/dimensions.py
@@ -1,1 +1,8 @@
-
+dimensions = (200,50)
+	# TUPLE is a list of immutable items
+	# That means that they cannt be changed
+	# defined as a list with () instead of []
+print(dimensions[0])
+print(dimensions[1])
+	# just printing it's items like in the list
+	
