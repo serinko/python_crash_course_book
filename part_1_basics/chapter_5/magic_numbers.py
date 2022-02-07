65defd534d24e970dd04c6d559454d34a9d7f959
serinko <johanesxsalmon@riseup.net>
2022-02-07 10:25:22 +0100

    statements != and == in practice

--- part_1_basics/chapter_5/magic_numbers.py
+++ part_1_basics/chapter_5/magic_numbers.py
@@ -5,3 +5,14 @@ if answer != 42:
     print('This is not the correct answer. Please try again!')
 # the correct answer is 42
 # so if user's input was not  42, print the message
+
+print("\n")
+
+answer = 42
+if answer == 42:
+    print("This is the correct answer, welcome.")
+else:
+    print('This is not the correct answer. Please try again!')
+# This would be the more common way to solve this numerical comparison
+# the former was to show how != works
+# But == is more common
