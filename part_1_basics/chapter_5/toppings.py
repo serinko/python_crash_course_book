125e419ec2e82d690543e073fe56b74c4331725c
serinko <johanesxsalmon@riseup.net>
2022-02-07 11:16:53 +0100

    if in list

--- part_1_basics/chapter_5/toppings.py
+++ part_1_basics/chapter_5/toppings.py
@@ -1,4 +1,4 @@
-requested_topping = 'mushroms'
+requested_topping = ['mushrooms', 'onions', 'pineapple']
 
-if requested_topping != 'anchovies':
-    print("Hold the anchovies!")
+if 'mushrooms' in requested_topping:
+    print("Add mushrooms.")
