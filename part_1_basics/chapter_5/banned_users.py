b0883124584973a86a991908bf53401a787c95e0
serinko <johanesxsalmon@riseup.net>
2022-02-07 11:25:39 +0100

    if not statement

--- part_1_basics/chapter_5/banned_users.py
+++ part_1_basics/chapter_5/banned_users.py
@@ -4,6 +4,6 @@ user = 'marie'
 # A new variable; user = marie
 
 if user not in banned_users:
-    print(user.title() + " you can post a response if you wish.")
+    print(user.title() + ", you can post a response if you wish.")
 # variable user is compared with the list
 # if it is NOT there - the message prints
