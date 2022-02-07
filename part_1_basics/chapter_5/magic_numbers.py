08c4c7c6523c49a177d95838ce9b7a0b2c0e4f02
serinko <johanesxsalmon@riseup.net>
2022-02-07 10:22:05 +0100

    statement != in practice

---
+++ part_1_basics/chapter_5/magic_numbers.py
@@ -0,0 +1,7 @@
+answer = 17
+# for example users input
+
+if answer != 42:
+    print('This is not the correct answer. Please try again!')
+# the correct answer is 42
+# so if user's input was not  42, print the message
