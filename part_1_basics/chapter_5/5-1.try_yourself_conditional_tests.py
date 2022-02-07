fddfe39618f6565f64ac8a59884fea0d2999ff93
serinko <johanesxsalmon@riseup.net>
2022-02-07 11:46:05 +0100

    excercise of comparisons

---
+++ part_1_basics/chapter_5/5-1.try_yourself_conditional_tests.py
@@ -0,0 +1,15 @@
+# EXCERCISE:
+# Write a series od conditional tests
+# print a statement describing each test and your precondition for the result
+# Look closely at the results
+# make sure you understand why each line evaluates True or False
+# Create 10 such tests. 5 True and 5 False
+
+car = 'subaru'
+print("Is car == 'subaru'?  I predict True.")
+print(car == 'subaru')
+# important detail - the print has no quotation but uses == as a\
+# variable comparison - so the print will be True or False
+
+print("\nIs car == 'audi'? I predict False.")
+print(car == 'audi')
