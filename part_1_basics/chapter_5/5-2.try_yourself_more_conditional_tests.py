a1f1bb8f805223579be39df90b6168e18f5a0c9e
serinko <johanesxsalmon@riseup.net>
2022-02-07 14:52:27 +0100

    more conditional tests

---
+++ part_1_basics/chapter_5/5-2.try_yourself_more_conditional_tests.py
@@ -0,0 +1,31 @@
+# EXCERCISE:
+# MAKE MORE COMPARISONS AND TESTS
+# Get some True and False results for following:
+# test for equality and inequality with strings
+# test using the lower() function
+# Numerical tests with ==;!=;<;>;<=;>=
+# using the and and or function with a keyword
+# Testing whether ite is in the list
+# Testing whether an item is not in the list
+
+number = 38
+user = 'matias'
+users_number = 23
+
+if users_number == number:
+    print("Dear " + user.title() + ", you are a winner of our lottery. \
+    Congratulation!")
+else:
+    print("Dear " + user.title() + ", we are sorry, but this is not " \
+                                   "the number. Do you want to try again?")
+
+print("\n")
+
+user = 'lilly'
+users_number = 38
+if users_number == number:
+    print("Dear " + user.title() + ", you are a winner of our lottery. " \
+                                   "Congratulation!")
+else:
+    print("Dear " + user.title() + ", we are sorry, but this is not " \
+                                   "the number. Do you want to try again?")
