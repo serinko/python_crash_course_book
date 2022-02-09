fa533653ccac9e07cf6fc8e15af2004bb3d92473
serinko <johanesxsalmon@riseup.net>
2022-02-09 14:26:42 +0100

    excercise application comrades

--- part_1_basics/chapter_5/5-2.try_yourself_more_conditional_tests.py
+++ part_1_basics/chapter_5/5-2.try_yourself_more_conditional_tests.py
@@ -179,16 +179,14 @@ user_name = 'diana'
 user_password = 'hgcyt.,:L1W!xxrt677'
 
 if user_name in list_comrades and user_password == comrade_password:
-    print(" This user is registered as a comrade with a username" +
+    print("This user is registered as a comrade with a username" +
           user_name.title() + ".  ==  ")
 
     print(user_name in list_comrades and \
           user_password == comrade_password)
 
-if user_name in list_comrades and \
-        user_password == xweser_password:
-    print(
-        " This user is registered as xweser comrades with a username " +
-        user_name + ".  ==  " \
-        + user_name in list_comrades and \
-        user_password == xweser_password)
+if user_name in list_comrades and user_password == xweser_password:
+    print("This user is registered as xweser comrades with a username: " +
+          user_name + ".  ==  " \
+          + user_name in list_comrades and \
+          user_password == xweser_password)
