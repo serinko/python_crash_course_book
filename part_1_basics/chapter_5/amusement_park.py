5a4386677d8a294fdc2bf093f7bbf4940bf937ce
serinko <johanesxsalmon@riseup.net>
2022-02-10 11:38:32 +0100

    if-elif-else chain

--- part_1_basics/chapter_5/amusement_park.py
+++ part_1_basics/chapter_5/amusement_park.py
@@ -4,14 +4,14 @@
 # anyone older 18 is $10
 # Use if statement to determine the rate
 
-age = 12
+age = 55
 if age < 4:
     print("Your admission cost is $0.")
 elif age < 18:
     print("Your admission cost is $5.")
 else:
     print("Your admission cost is $10.")
-    
+
 # if-elif_else: chain, in practice:
 # if: True, code gets executed and Python skips the rest
 # if: False, code reads elif
