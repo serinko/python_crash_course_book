0f2d508b8ccf8ae31eb9dbcbe4015726a7aff01e
serinko <johanesxsalmon@riseup.net>
2022-02-10 12:52:42 +0100

    multiple if functions

--- part_1_basics/chapter_5/toppings.py
+++ part_1_basics/chapter_5/toppings.py
@@ -1,4 +1,23 @@
+# multiple conditions
+# in case there is more then one, more if tatements needs to be checked
+
 requested_topping = ['mushrooms', 'onions', 'pineapple']
 
 if 'mushrooms' in requested_topping:
     print("Add mushrooms.")
+
+if 'onions' in requested_topping:
+    print("Add onions.")
+
+if 'pineapple' in requested_topping:
+    print("Add pineapple.")
+
+if 'pepperoni' in requested_topping:
+    print("Add pepperoni.")
+
+if 'extra cheese' in requested_topping:
+    print("Add extra cheese.")
+
+# endless if statements can be added and as list gets modified, message changes
+
+print("\nFinished your pizza.")
