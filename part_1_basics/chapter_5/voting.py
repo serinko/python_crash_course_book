da2d2a295a122d15349217bdc7897da0554e86d4
serinko <johanesxsalmon@riseup.net>
2022-02-10 11:18:45 +0100

    if statements

--- part_1_basics/chapter_5/voting.py
+++ part_1_basics/chapter_5/voting.py
@@ -1,4 +1,10 @@
-age = 19
+age = 7
 if age >= 18:
     print("You are old enough to vote.")
     # as any if statement - the statement must be true
+    print("Have you registered to vote yet?")
+else:
+    print("Sorry, you are too young to vote.")
+    print("Please register to vot as soon as you turn 18!")
+    # As it suggested in the name, in case if statement doesnt pass True
+    # else: will happen
