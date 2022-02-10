966b9d2f81b4cfcdbc6d890721496981b60d7f0e
serinko <johanesxsalmon@riseup.net>
2022-02-10 14:07:43 +0100

    if-elif excercise

---
+++ part_1_basics/chapter_5/5-5.try_yourself_alein_colors_#3.py
@@ -0,0 +1,8 @@
+alien_color = 'green'
+
+if alien_color == 'red':
+    print("You earned 5 points")
+elif alien_color == 'yellow':
+    print("You earned 10 points.")
+elif alien_color == 'green':
+    print("you earned 15 points.")
