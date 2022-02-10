ff4878851faea2cc0e0555aab2e1f35854de8e53
serinko <johanesxsalmon@riseup.net>
2022-02-10 14:17:17 +0100

    if-elif-else function

---
+++ part_1_basics/chapter_5/5-6.try_yourself_stages_of _life.py
@@ -0,0 +1,16 @@
+age = 43
+
+print("The age of this person is " + str(age) + ".")
+
+if age < 2:
+    print("This is a baby.")
+elif age < 4:
+    print("This is a toddler.")
+elif age < 13:
+    print("This is a kid")
+elif age < 30:
+    print("This is a teenager.")
+elif age < 65:
+    print("This is an adult.")
+else:
+    print("This is an elder.")
