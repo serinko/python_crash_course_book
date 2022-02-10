f96c0a0b945c5a71629d7444d3175896ed950c25
serinko <johanesxsalmon@riseup.net>
2022-02-10 14:55:05 +0100

    if-else inside for loop

--- part_1_basics/chapter_5/toppings.py
+++ part_1_basics/chapter_5/toppings.py
@@ -1,22 +1,41 @@
 # multiple conditions
 # in case there is more then one, more if tatements needs to be checked
 
-requested_topping = ['mushrooms', 'onions', 'pineapple']
+requested_toppings = ['mushrooms', 'onions', 'pineapple', 'pepperoni']
 
-if 'mushrooms' in requested_topping:
+if 'mushrooms' in requested_toppings:
     print("Add mushrooms.")
 
-if 'onions' in requested_topping:
+if 'onions' in requested_toppings:
     print("Add onions.")
 
-if 'pineapple' in requested_topping:
+if 'pineapple' in requested_toppings:
     print("Add pineapple.")
 
-if 'pepperoni' in requested_topping:
+if 'pepperoni' in requested_toppings:
     print("Add pepperoni.")
 
-if 'extra cheese' in requested_topping:
+if 'extra cheese' in requested_toppings:
     print("Add extra cheese.")
 # endless if statements can be added and as list gets modified, message changes
 # They get evaluated regardless of the previous if result
+print("\nFinished your pizza.")
+
+print("\n")
+
+# More efficient way would be the for loop:
+for requested_topping in requested_toppings:
+    print("Adding " + requested_topping + ".")
+
+print("\nFinished your pizza.")
+
+print("\n")
+
+# Pepperoni ran out:
+for requested_topping in requested_toppings:
+    if requested_topping == "pepperoni":
+        print("We are sorry but we are out of pepperoni.")
+    else:
+        print("Adding " + requested_topping + ".")
+
 print("\nFinished your pizza.")
