00d85daaa64a942b5a065dd9d1a64ab56f57cca5
serinko <johanesxsalmon@riseup.net>
2022-02-10 14:37:16 +0100

    multiple ifs

---
+++ part_1_basics/chapter_5/5-7.try-yourself-favourite_fruit.py
@@ -0,0 +1,16 @@
+favorite_fruits = ['strawberry', 'banana', 'avocado', 'pineapple', ]
+
+if 'banana' in favorite_fruits:
+    print("I like bananas.")
+
+if 'apple' in favorite_fruits:
+    print("I like apple.")
+
+if 'avocado' in favorite_fruits:
+    print("I like avocado.")
+
+if 'strawberry' in favorite_fruits:
+    print("I like strawberry.")
+
+if 'peach' in favorite_fruits:
+    print("I like peach.")
