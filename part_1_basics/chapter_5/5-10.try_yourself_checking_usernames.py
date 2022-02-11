8d35aeeaa6fb462306d09bbeb0b6d47d48e6cfd3
serinko <johanesxsalmon@riseup.net>
2022-02-11 10:14:11 +0100

    excercise finished

--- part_1_basics/chapter_5/5-10.try_yourself_checking_usernames.py
+++ part_1_basics/chapter_5/5-10.try_yourself_checking_usernames.py
@@ -1,5 +1,5 @@
 current_users = ['admin', 'francesca', 'eva', 'ivan', 'rick', 'mellisa']
-new_users = ['clemence', 'EVA' 'ivan', 'emily', 'steve']
+new_users = ['clemence', 'EVA', 'ivan', 'emily', 'steve', 'mellisa']
 
 if new_users:
     for user in new_users:
@@ -13,6 +13,7 @@ if new_users:
                 "Hello, you have created an account with a username " +
                 user.title() + "."
             )
+        print("\n")
 
 else:
     print("The space for username must be filled to register.")
