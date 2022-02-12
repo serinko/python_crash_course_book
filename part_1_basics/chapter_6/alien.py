1d247503e32f36405f35ed92e81e70afea2f6a96
serinko <johanesxsalmon@riseup.net>
2022-02-12 16:23:21 +0100

    moving alien

--- part_1_basics/chapter_6/alien.py
+++ part_1_basics/chapter_6/alien.py
@@ -33,5 +33,31 @@ alien_0['color'] = 'yellow'
 
 print("The alien is now " + alien_0['color'] + ".")
 
+print("\n")
+
 # LET'S BE DYNAMIC
 # We can move the alien
+
+alien_0['speed'] = 'medium'
+print(
+    "Original x-position: " + str(alien_0['x_position'])
+)
+# ~ print(alien_0)
+
+print("\n")
+# LEt;s move alien to the right
+if alien_0['speed'] == 'slow':
+    x_increment = 1
+elif alien_0['speed'] == 'medium':
+    x_increment = 2
+else:
+    # This is a fast moving alien
+    x_increment = 3
+
+# The new position is the old position plus the increment
+alien_0['x_position'] = alien_0['x_position'] + x_increment
+
+print(
+    "New x-position: " +
+    str(alien_0['x_position'])
+)
