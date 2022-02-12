feccfe68a3ba0850d85ba88576337f9105ae4f9f
serinko <johanesxsalmon@riseup.net>
2022-02-12 10:22:42 +0100

    starting code dictionaries

--- part_1_basics/chapter_6/alien.py
+++ part_1_basics/chapter_6/alien.py
@@ -1,8 +1,8 @@
 alien_0 = {'color': 'green', 'points': 5}
 # alien_0 is a dictionary
-# {} define the variables and values in the dictionary
+# {} define the key and connected values in the dictionary
 
 print(alien_0['color'])
 print(alien_0['points'])
-# print(dictionary['variable'])
-# this will print the value of the variable in the given dictionary
+# print(dictionary['key'])
+# this will print the value of the key in the given dictionary
