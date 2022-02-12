90eb47e513913bb1ad6b6594706d71e98b851622
serinko <johanesxsalmon@riseup.net>
2022-02-12 10:34:09 +0100

    starting code dictionaries

--- part_1_basics/chapter_6/alien.py
+++ part_1_basics/chapter_6/alien.py
@@ -1,3 +1,4 @@
+# dictionary{key:value}
 alien_0 = {'color': 'green', 'points': 5}
 # alien_0 is a dictionary
 # {} define the key and connected values in the dictionary
@@ -6,3 +7,6 @@ print(alien_0['color'])
 print(alien_0['points'])
 # print(dictionary['key'])
 # this will print the value of the key in the given dictionary
+# key value can be: a number, string, list, another dictionary - any object
+# dictionary = {key:value} or multiple {key_0:value_0, key_1:value_1}
+# "function/call(dictionary[key])" returns: "value"
