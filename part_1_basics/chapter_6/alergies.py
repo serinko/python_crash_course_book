db6f030ab17ef6d1990fc5cdfa3fdca2a621bd6f
serinko <johanesxsalmon@riseup.net>
2022-02-13 20:23:38 +0100

    quick dictionary of ppls alergies

--- part_1_basics/chapter_6/alergies.py
+++ part_1_basics/chapter_6/alergies.py
@@ -1,12 +1,12 @@
-alergies_diets = {
+allergies_diets = {
     'ben': ['soya', 'gluten', 'cucumber'],
     'lena': ['gluten', 'vegetarian diet'],
     'percival': ['vegan diet', 'nuts'],
     'clara': ['keto diet', 'no carbs', 'no starch', 'alergy beans', ],
 }
 
-for name, diet in alergies_diets.items():
-    print(f"{name.title()}'s diet, alergies and food info:")
+for name, diet in allergies_diets.items():
+    print(f"{name.title()}'s diet, allergies and food info:")
     for food in diet:
         print(f"\t- {food}")
     print("\n")
