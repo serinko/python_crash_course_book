49241a222aac3d592eedb15b4978fe699939b3c1
serinko <johanesxsalmon@riseup.net>
2022-02-13 10:34:25 +0100

    get() method for possible no value key

--- part_1_basics/chapter_6/alien_no_points.py
+++ part_1_basics/chapter_6/alien_no_points.py
@@ -5,5 +5,15 @@ alien_0 = {
 }
 
 # print value, which is not defined
-print(alien_0['points'])
+# ~ print(alien_0['points'])
 # gives KeyError: 'points'
+
+# get() method
+point_value = alien_0.get('points', 'No point value asigned.')
+print(point_value)
+# If the key 'points' exists - we get the corresponding value
+# if does not exist, we get the 'default' value
+# message gets printed clean without any error
+
+# If there is a chance the key you are asking for has no value
+# consider using get() method
