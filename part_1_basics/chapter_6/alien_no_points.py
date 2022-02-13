8b87cff561bf87fda47a49088317f3cdfe21c6f0
serinko <johanesxsalmon@riseup.net>
2022-02-13 10:29:23 +0100

    keyerror - no key defined

---
+++ part_1_basics/chapter_6/alien_no_points.py
@@ -0,0 +1,9 @@
+# Using get() to access value
+alien_0 = {
+    'color': 'green',
+    'speed': 'slow',
+}
+
+# print value, which is not defined
+print(alien_0['points'])
+# gives KeyError: 'points'
