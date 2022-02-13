777c9cdb213773f71c3b77443dcc83ff412a87d2
serinko <johanesxsalmon@riseup.net>
2022-02-13 15:39:41 +0100

    item() function to return key-value pair

---
+++ part_1_basics/chapter_6/user.py
@@ -0,0 +1,19 @@
+user_0 = {
+    'username': 'efermi',
+    'first': 'enrico',
+    'last': 'fermi',
+}
+
+# items() function returns a list key-value pairs
+# the list assigns the pair to the provided variables in the statement
+# any name for the variables can be chosen
+for key, value in user_0.items():
+    print(f"\nkey: {key}")
+    print(f"Value: {value}")
+
+print("\n")
+
+# simple version of this example:
+for key, value in user_0.items():
+    print(key)
+    print(value)
