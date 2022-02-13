24e2a09ad7deeb21ac26d7bc9c635bf4e7c9a3b0
serinko <johanesxsalmon@riseup.net>
2022-02-13 11:44:01 +0100

    glossary of programming words

---
+++ part_1_basics/chapter_6/6-3.try_yourself_glossary.py
@@ -0,0 +1,7 @@
+programming_glossary = {
+    'print()': 'Prints a string.',
+    '.append()': 'Add a value to the list.',
+    'del ': 'Permanently removes an item.',
+    'for loop': 'For each item - do action.',
+    'iif-else': 'Statement choosing of two possibilities.'
+}
