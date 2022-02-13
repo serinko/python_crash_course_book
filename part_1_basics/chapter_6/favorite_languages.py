9a3d370f25cd317eb84f3282dc8751357bbfe620
serinko <johanesxsalmon@riseup.net>
2022-02-13 09:30:12 +0100

    a dictionary of similar objects

---
+++ part_1_basics/chapter_6/favorite_languages.py
@@ -0,0 +1,11 @@
+# Dictionary with similar objects
+# object = {many informations} or
+# one_kind_of_information = {many objects}
+# This is a dictionary of similar objects, one information
+
+favorite_languages = {
+    'jen': 'pythonn',
+    'sarah': 'c',
+    'edward': 'ruby',
+    'pjil': 'python',
+}
