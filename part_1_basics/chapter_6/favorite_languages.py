077d62a5cf02f2073951494e4a176ef60a1b578a
serinko <johanesxsalmon@riseup.net>
2022-02-13 09:47:35 +0100

    a dictionary of similar objects

--- part_1_basics/chapter_6/favorite_languages.py
+++ part_1_basics/chapter_6/favorite_languages.py
@@ -9,3 +9,8 @@ favorite_languages = {
     'edward': 'ruby',
     'phil': 'python',
 }
+language = favorite_languages['sarah'].title()
+# new variable = dictionary[key], this pulls the value and styles with title()
+print(f"Sarah's favorite language is {language}")
+# It is easier and nicer to rrad to print with a variable.
+# For the first time we can see that we use {} in print instead of +
