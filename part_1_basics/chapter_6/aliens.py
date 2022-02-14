10f294d64964d3a2107b989b5eda451e8c247c82
serinko <johanesxsalmon@riseup.net>
2022-02-14 11:23:49 +0100

    code to make multiple dictionaries

--- part_1_basics/chapter_6/aliens.py
+++ part_1_basics/chapter_6/aliens.py
@@ -1,20 +1,40 @@
-alien_0 = {
-    'color': 'green',
-    'points': 5,
-}
+# alien_0 = {
+#     'color': 'green',
+#     'points': 5,
+# }
+#
+# alien_1 = {
+#     'color': 'yellow',
+#     'points': 10,
+# }
+#
+# alien_2 = {
+#     'color': 'red',
+#     'points': 15,
+# }
+#
+# aliens = [alien_0, alien_1, alien_2, ]
+#
+# for alien in aliens:
+#     print(alien)
 
-alien_1 = {
-    'color': 'yellow',
-    'points': 10,
-}
 
-alien_2 = {
-    'color': 'red',
-    'points': 15,
-}
+# Let's make a code generating aliens
+aliens = []
+# startinf with an empty list
 
-aliens = [alien_0, alien_1, alien_2, ]
+# generate 30 green aliens
+for alien in range(30):
+    new_alien = {
+        'color': 'green',
+        'points': 5,
+        'speed': 'slow',
+    }
+    aliens.append(new_alien)
 
-for alien in aliens:
+# show the first 5 aliens
+for alien in aliens[:5]:
     print(alien)
-   
+print("...")
+
+# Show how many aliend have been created
