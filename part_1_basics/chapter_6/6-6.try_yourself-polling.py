1ef09c575cb0b35f72c6d16c90c428508bf7ba0f
serinko <johanesxsalmon@riseup.net>
2022-02-14 10:21:23 +0100

    excercise finished

--- part_1_basics/chapter_6/6-6.try_yourself-polling.py
+++ part_1_basics/chapter_6/6-6.try_yourself-polling.py
@@ -5,4 +5,17 @@ favorite_languages = {
     'phil': 'python',
 }
 
-poll_of_people
+poll_of_people = ['edward', 'erin', 'mariana', 'isac', 'anna', ]
+
+for name in favorite_languages.keys():
+
+    if name in poll_of_people:
+        print(f"\nHey {name.title()}, "
+              f"we would like to invite you to our poll "
+              f"of favorite languages.")
+    else:
+        language = favorite_languages[name]
+        print(
+            f"\nHey {name.title()}, thank you for joining the poll. "
+            f"We are happy to see that you like to use {language.title()}."
+        )
