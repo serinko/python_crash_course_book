cdeed4c4dc23a3d1123fcb08fa89b36c6b6c0a8f
serinko <johanesxsalmon@riseup.net>
2022-02-15 09:26:07 +0100

    changing values of a slice of a list

--- part_1_basics/chapter_6/aliens.py
+++ part_1_basics/chapter_6/aliens.py
@@ -24,12 +24,15 @@ aliens = []
 # startinf with an empty list
 
 # generate 30 green aliens
+# using range tells Python how many times to loop through the loop
 for alien_number in range(30):
+    # run 30 times the loop in which you crade such dictionary
     new_alien = {
         'color': 'green',
         'points': 5,
         'speed': 'slow',
     }
+    # and append it to the list of dictionaries
     aliens.append(new_alien)
 
 # show the first 5 aliens
@@ -37,11 +40,14 @@ for alien in aliens[:5]:
     print(alien)
 print("...")
 
-# Show how many aliens have been created
+# Show how many aliens have been created using function len()
 print(f"Total number of aliens: {len(aliens)}\n")
 
 for alien in aliens[:3]:
+    # for every item in a dictionary [slice 0,1,2]:
     if alien['color'] == 'green':
+        # if item[key] has this 'value'
+        # change these keys to these values
         alien['color'] = 'yellow'
         alien['speed'] = 'medium'
         alien['points'] = '10'
