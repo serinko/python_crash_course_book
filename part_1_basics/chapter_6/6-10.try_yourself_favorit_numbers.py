4c460566b5e2c65c599d44cd29b4bce3039482af
serinko <johanesxsalmon@riseup.net>
2022-02-15 15:05:25 +0100

    printing list nesting in a dictionary

---
+++ part_1_basics/chapter_6/6-10.try_yourself_favorit_numbers.py
@@ -0,0 +1,17 @@
+favorite_numbers = {
+    'wanderer': [0, 1, 2, 3],
+    'magician': [1, 2],
+    'seer': [2, 13, 20],
+    'temperance': [14, 21],
+    'death': [13],
+
+}
+
+for character, numbers in favorite_numbers.items():
+    if len(numbers) == 1:
+        print(f" This is {character.title()}'s favorite number: {number} ")
+
+    else:
+        print(f"These are the {character.title()}'s favorite numbers:")
+        for number in numbers:
+            print(f"\t{(number)}")
