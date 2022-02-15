cb70fb23d3ec45c8ed7a20ffeda642664045af2b
serinko <johanesxsalmon@riseup.net>
2022-02-15 09:56:01 +0100

    list items as a value in a dictionary

--- part_1_basics/chapter_6/pizza.py
+++ part_1_basics/chapter_6/pizza.py
@@ -3,10 +3,13 @@ pizza = {
     'crust': 'thick',
     'toppings': ['mushrooms', 'extra cheese', ],
 }
+# second value in the dictionary is a list
 
 # Summarize the order
 print(f"You ordered a {pizza['crust']}-crust pizza "
       f"with the fillowing toppings:")
 
+# for loop the items
+# point to the list as to any value of a dictionary
 for topping in pizza['toppings']:
     print(f"\t{topping.title()}")
