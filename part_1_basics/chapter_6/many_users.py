7155e231d3a29bec3c95d34b81625fae9135a043
serinko <johanesxsalmon@riseup.net>
2022-02-15 10:59:09 +0100

    nesting dictionaries in a dictionary

--- part_1_basics/chapter_6/many_users.py
+++ part_1_basics/chapter_6/many_users.py
@@ -20,3 +20,11 @@ for username, user_info in users.items():
 
     print(f"\tFull name: {full_name.title()}")
     print(f"\tLocation: {location.title()}")
+
+# in a for loop we have key, value in the dictionary
+# we print the key as a username
+# The value we know is another dictionary
+# define full name as a value_2nd dictionary[key] ('first') and
+# value_2n_dictionary[key] ('last)
+# the same with the location
+# then we simply print these
