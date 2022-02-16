0e259933c9305d356418f0070714d474ed5f2c3b
serinko <johanesxsalmon@riseup.net>
2022-02-16 09:13:25 +0100

    template

---
+++ part_1_basics/chapter_6/6-11.try-yourself-cities.py
@@ -0,0 +1,17 @@
+cities = {
+    'city_0': {
+        'country': '',
+        'population': '',
+        'imteresting_fact': '',
+    },
+    'city_1': {
+        'country': '',
+        'population': '',
+        'imteresting_fact': '',
+    },
+    'city_2': {
+        'country': '',
+        'population': '',
+        'imteresting_fact': '',
+    },
+}
