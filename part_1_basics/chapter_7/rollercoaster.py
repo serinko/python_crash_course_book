a924cec0530c43dfadba5b4a0c53e98fffa0a30e
serinko <johanesxsalmon@riseup.net>
2022-02-17 10:05:18 +0100

    int() function

---
+++ part_1_basics/chapter_7/rollercoaster.py
@@ -0,0 +1,7 @@
+height = input("How tall are you, in cm? ")
+height = int(height)
+
+if height >= 125:
+    print("\nYou are tall enough to ride, enjoy!")
+else:
+    print(f"You are too little. Grow up. Now go away!!")
