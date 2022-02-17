7dc39effd2205ff0e501804ee7e652a3a4f8adf9
serinko <johanesxsalmon@riseup.net>
2022-02-17 13:02:30 +0100

    while loops for user

--- part_1_basics/chapter_7/parrot.py
+++ part_1_basics/chapter_7/parrot.py
@@ -3,7 +3,7 @@
 
 # user will decide when to quit, using the while loop
 prompt = "\nTell me something, and I will repeat it back to you:"
-prompt += "\nEnter '' to end the program. "
+prompt += "\nEnter 'quit' to end the program. "
 message = ""
 active = True
 
