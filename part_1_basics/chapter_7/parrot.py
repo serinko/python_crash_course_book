0b8f947d22fb322f0fce9ff006c3634ef1d45375
serinko <johanesxsalmon@riseup.net>
2022-02-17 13:01:39 +0100

    flag in while loops

--- part_1_basics/chapter_7/parrot.py
+++ part_1_basics/chapter_7/parrot.py
@@ -3,9 +3,14 @@
 
 # user will decide when to quit, using the while loop
 prompt = "\nTell me something, and I will repeat it back to you:"
-prompt += "\nEnter 'quit' to end the program. "
+prompt += "\nEnter '' to end the program. "
 message = ""
+active = True
 
-while message != 'quit':
-    message = input(prompt)
-    print(message)
+while active:
+    message == input(prompt)
+
+    if message == 'quit':
+        active = False
+    else:
+        print(message)
