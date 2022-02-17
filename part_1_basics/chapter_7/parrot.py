55d476eb6eb422e98821fbe326c5e304cb030cff
serinko <johanesxsalmon@riseup.net>
2022-02-17 13:06:00 +0100

    while loops for user

--- part_1_basics/chapter_7/parrot.py
+++ part_1_basics/chapter_7/parrot.py
@@ -6,6 +6,9 @@ prompt = "\nTell me something, and I will repeat it back to you:"
 prompt += "\nEnter 'quit' to end the program. "
 message = ""
 active = True
+# Such variable is called flag
+# We can use it in a statement
+# and by if statements define if True/False
 
 while active:
     message = input(prompt)
