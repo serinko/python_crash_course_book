7d83e94fa633b3b62b310808a4eb0950cb7068e8
serinko <johanesxsalmon@riseup.net>
2022-02-17 09:49:38 +0100

    first commit

--- part_1_basics/chapter_7/greeter.py
+++ part_1_basics/chapter_7/greeter.py
@@ -5,3 +5,4 @@ prompt = "If you tell us who you are, we can personalize the message you see."
 prompt += "\nWhat's your first name? "
 name = input(prompt)
 print(f"Hello, {name.title()}.")
+# the + sign takes the string and adds a new string onto its end
