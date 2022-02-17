d1e72bba37d71e391c4fe2974cc855601d0f8416
serinko <johanesxsalmon@riseup.net>
2022-02-17 10:11:19 +0100

    int() function

--- part_1_basics/chapter_7/rollercoaster.py
+++ part_1_basics/chapter_7/rollercoaster.py
@@ -1,5 +1,6 @@
 height = input("How tall are you, in cm? ")
 height = int(height)
+# When you us enumerical values, make sure to use int()
 
 if height >= 125:
     print("\nYou are tall enough to ride, enjoy!")
