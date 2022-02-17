32767e36f4c2835c9b640836342641b40db639b1
serinko <johanesxsalmon@riseup.net>
2022-02-17 11:04:59 +0100

    while loops

---
+++ part_1_basics/chapter_7/counting.py
@@ -0,0 +1,11 @@
+# while loop, runs through a condition as long as the statement is true
+current_number = 1
+while current_number <= 5:
+    print(current_number)
+    current_number += 1
+# in first line we start counting from 1
+# the while loop is set to run as long sas the number <= 5
+# the code inside the while loop prints the current number and then
+# (+=) adds 1, making the current_number value = 2
+# compraes it with the while parameters and repeats until
+# the while statement is not true
