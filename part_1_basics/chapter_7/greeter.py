d1250dd3d7405f4cb4d2480106d897a1fc45d6ab
serinko <johanesxsalmon@riseup.net>
2022-02-17 09:38:07 +0100

    input(prompt) function

---
+++ part_1_basics/chapter_7/greeter.py
@@ -0,0 +1,7 @@
+# name = input("Please eneter your name: ")
+# print(f"Hello, {name.title()}.")
+
+prompt = "If you tell us who you are, we can personalize the message you see."
+prompt += "\nWhat's your first name? "
+name = input(prompt)
+print(f"Hello, {name.title()}.")
