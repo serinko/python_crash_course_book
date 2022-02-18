f0bbadfedd27b2f779fe143e6379c012a989e9ec
serinko <johanesxsalmon@riseup.net>
2022-02-18 13:22:09 +0100

    two lists and while loop

---
+++ part_1_basics/chapter_7/confirmed_users.py
@@ -0,0 +1,16 @@
+# MOVING ITEMS FORM ONE LIST TO ANOTHER
+# Start with users that need to be verified
+# and an empty list to hold confirmed users.
+
+unconfirmed_users = ['alice', 'brendan', 'shane']
+confirmed_users = []
+
+# Verify each user until there ar eno more unconfirmed users.
+# Move each verified user into the list of confirmed users.
+
+while unconfirmed_users:
+    # Means the loop is True as long as the list has items in it
+    current_user = unconfirmed_users.pop()
+
+    print(f"Verifying user: {current_user.title()}")
+    confirmed_users.append(current_user)
