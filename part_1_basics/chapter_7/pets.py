cff909a950fb7841c7fcbc15ff4907195a19844f
serinko <johanesxsalmon@riseup.net>
2022-02-18 13:40:05 +0100

    removing all specific instances from a list

---
+++ part_1_basics/chapter_7/pets.py
@@ -0,0 +1,13 @@
+# Removing all instances of specific values from a list
+
+pets = [
+    'dog', 'cat', 'dog', 'goldfish', 'cat', 'rabbit', 'cat',
+]
+
+print(pets)
+
+while 'cat' in pets:
+    pets.remove('cat')
+# remove function will run until while statement != True
+
+print(pets)
