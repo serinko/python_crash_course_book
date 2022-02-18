06d290298a4ffb6cf79d7532dbdc0ebac4e10bac
serinko <johanesxsalmon@riseup.net>
2022-02-18 11:02:12 +0100

    excercise finished

--- part_1_basics/chapter_7/7-5.try_yourself_movie_tickets.py
+++ part_1_basics/chapter_7/7-5.try_yourself_movie_tickets.py
@@ -2,3 +2,19 @@
 # under 3 years, ticket is free
 # between 3 and 12, ticket is 10$
 # over 12, ticket is $10
+
+prompt = "Welcome to our cinema. How old are you? "
+age = input(prompt)
+age = int(age)
+
+if age < 121:
+    if age < 3:
+        price = 0
+    elif age <= 12:
+        price = 10
+    elif age <= 120:
+        price = 15
+    message = "Your ticket price is: "
+    print(f"{message}{str(price)}")
+else:
+    print("Do you think we are stupid? Make fun of yourself!")
