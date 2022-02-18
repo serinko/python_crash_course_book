a6ab2a0f3d5e2dfda65bfdb392a6b2e5c12cf4b0
serinko <johanesxsalmon@riseup.net>
2022-02-18 11:59:27 +0100

    excercise done

---
+++ part_1_basics/chapter_7/7-7.try_yourself_infinity.py
@@ -0,0 +1,6 @@
+prompt = "\nPres any letter to run an infinite loop."
+prompt += "\nTo quit, press ctrl+c."
+message = input(prompt)
+x = True
+while x:
+    print("This will make you crazy!!!!!!!!!!!!")
