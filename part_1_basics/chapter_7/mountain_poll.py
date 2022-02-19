e7f74190bcc4dc8a7e59eac87bd6b0440cf0cce1
serinko <johanesxsalmon@riseup.net>
2022-02-19 09:28:49 +0100

    filling dictionary, using while loop

---
+++ part_1_basics/chapter_7/mountain_poll.py
@@ -0,0 +1,21 @@
+responses = {}
+
+# set a flag to indicate the polling is active
+poling_active = True
+
+while polling_active:
+    # Prompt for the persons name and response
+    name = input("\n\nWhat is your name? ")
+    response = input("\nWhich mountain would you like to climb today? ")
+
+    # Store response in a dictionary
+    responses[name] = response
+
+    # Find out if anyone else is going to take the poll
+    repeat = input("\n\nWould you like to let another person respond?"
+                   "(yes/ no)")
+
+    if repeat == 'no':
+        polling_active = False
+
+# Polling is complete - show the results
