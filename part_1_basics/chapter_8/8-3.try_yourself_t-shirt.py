f98d974799f8c617f8598e569bd52b2354fe58e6
serinko <johanesxsalmon@riseup.net>
2022-02-20 09:30:39 +0100

    excercise functions and calls

---
+++ part_1_basics/chapter_8/8-3.try_yourself_t-shirt.py
@@ -0,0 +1,7 @@
+def make_shirt(size, front_print):
+    """Displays T-shirt summary message"""
+    print(f"The t-shirt size is: {size.upper()}.")
+    print(f"The chosen print is: {front_print}.")
+
+
+make_shirt('m', 'a.c.a.b.')
