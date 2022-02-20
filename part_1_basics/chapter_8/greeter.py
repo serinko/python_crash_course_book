9fbb0c2e85f946ffaf1cfdd5bc63274d4d160a3e
serinko <johanesxsalmon@riseup.net>
2022-02-20 08:25:51 +0100

    def function_name():

--- part_1_basics/chapter_8/greeter.py
+++ part_1_basics/chapter_8/greeter.py
@@ -1,6 +1,12 @@
+# The function definition using def funtion_name():
+# Function definition is marked with the colon in the end
+
 def greet_user():
     """Display a simple greeting."""
     print("Hello!")
+    # All the indented lines make the body of the function
+    # The """ text """ is a "docstring" - describing what function does
 
 
 greet_user()
+# A call for the function - Python executes that
