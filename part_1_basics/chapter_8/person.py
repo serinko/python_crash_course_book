a164fe14f9b4e4a712c153758a6aeef8f54ced7c
serinko <johanesxsalmon@riseup.net>
2022-02-20 10:54:46 +0100

    returning a dictionary + many optional arguments

--- part_1_basics/chapter_8/person.py
+++ part_1_basics/chapter_8/person.py
@@ -33,3 +33,36 @@ def build_person(first_name, last_name, age=None):
 musician = build_person('jimi', 'hendrix', age=27)
 # We defining parameter and argument age=27
 print(musician)
+
+
+# This way we can be opened for any type of added parameters to dictionaries
+# Let's try to add myself:
+
+def build_person(first_name, last_name, age=None, country=None,
+                 occupation=None):
+    """Return a dictionary of information about a person."""
+    person = {
+        'first': first_name,
+        'last': last_name,
+    }
+    if age:
+        person['age'] = age
+    if country:
+        person['country'] = country
+    if type:
+        person['occupation'] = occupation
+    return person
+
+
+famos_person = \
+    build_person(
+        'emma',
+        'goldman',
+        age=None,
+        country='usa',
+        occupation='revolutionary'
+    )
+
+print(famos_person)
+for parameter, argument in famos_person.items():
+    print(f"{parameter}: {argument.title()}")
