a7d1fd42caf3c354dd2fedafdeaee64535947d0a
serinko <johanesxsalmon@riseup.net>
2022-02-20 09:06:15 +0100

    keyword arguments

--- part_1_basics/chapter_8/pets.py
+++ part_1_basics/chapter_8/pets.py
@@ -9,3 +9,11 @@ describe_pet('hamster', 'harry')
 # Function can be called multiple times
 # Using new arguments for the same parameters:
 describe_pet('dog', 'psouk')
+# ORDER IS IMPORTANT!
+
+# keyword arguments
+describe_pet(animal_type='hamster', pet_name='harry')
+# If we define the keywords
+# to which parameter, given argumet is asigned, order can be changed
+# Python prioritize the keyword defintion in this case
+describe_pet(pet_name='harry', animal_type='hamster')
