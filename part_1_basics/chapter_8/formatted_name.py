9cb96c66f7231f8b1dc1ac6e4ede7c674a7fe2f0
serinko <johanesxsalmon@riseup.net>
2022-02-20 10:03:41 +0100

    making an argument optional

--- part_1_basics/chapter_8/formatted_name.py
+++ part_1_basics/chapter_8/formatted_name.py
@@ -6,3 +6,21 @@ def get_formatted_name(first_name, last_name):
 
 musician = get_formatted_name('jimi', 'hendrix')
 print(musician)
+
+
+# OPTIONAL ARGUMENT
+# Adding an empty parameter as a default
+# That can be redefined or left as it is
+# if we did not defined it empty, anytime a person has no middle name..
+# We recieve error
+def get_formatted_name(first_name, last_name, middle_name=''):
+    """Return a full ame, neatly fortmatted"""
+    full_name = f"{first_name} {middle_name} {last_name}"
+    return full_name.title()
+
+
+musician = get_formatted_name('jimi', 'hendrix')
+print(musician)
+
+musician = get_formatted_name('john', 'hooker', 'lee')
+print(musician)
