c39f655488d2c37dad6d0a7adbe05b7aca20c70c
serinko <johanesxsalmon@riseup.net>
2022-02-20 10:41:04 +0100

    returning a dictionary + optional argument

--- part_1_basics/chapter_8/person.py
+++ part_1_basics/chapter_8/person.py
@@ -10,3 +10,25 @@ def build_person(first_name, last_name):
 
 musician = build_person('jimi', 'hendrix')
 print(musician)
+
+
+# This way makes our data stored in parameter as well as in dictionary
+# we can then easily modify for optional return
+
+def build_person(first_name, last_name, age=None):
+    # in case of numbers we dont define empty string but None as a default
+    """Return a dictionary of information about a person."""
+    person = {
+        'first': first_name,
+        'last': last_name,
+    }
+    if age:
+        person['age'] = age
+    # if age is given == True:
+    # dictionary person will add a key 'age' with the value of age
+    return person
+
+
+musician = build_person('jimi', 'hendrix', age=27)
+# We defining parameter and argument age=27
+print(musician)
