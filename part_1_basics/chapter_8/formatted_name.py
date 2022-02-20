7968d0ffe8b42d67821883f20468ae18a9550aa4
serinko <johanesxsalmon@riseup.net>
2022-02-20 10:06:41 +0100

    making an argument optional with if-else

--- part_1_basics/chapter_8/formatted_name.py
+++ part_1_basics/chapter_8/formatted_name.py
@@ -15,7 +15,10 @@ print(musician)
 # We recieve error
 def get_formatted_name(first_name, last_name, middle_name=''):
     """Return a full ame, neatly fortmatted"""
-    full_name = f"{first_name} {middle_name} {last_name}"
+    if middle_name:
+        full_name = f"{first_name} {middle_name} {last_name}"
+    else:
+        full_name = f"{first_name} {last_name}"
     return full_name.title()
 
 
