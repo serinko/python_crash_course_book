18954142aaddb25f868f15a9d86d9655512a7eb5
serinko <johanesxsalmon@riseup.net>
2022-02-20 09:32:11 +0100

    excercise finished

--- part_1_basics/chapter_8/8-3.try_yourself_t-shirt.py
+++ part_1_basics/chapter_8/8-3.try_yourself_t-shirt.py
@@ -1,7 +1,8 @@
 def make_shirt(size, front_print):
     """Displays T-shirt summary message"""
-    print(f"The t-shirt size is: {size.upper()}.")
-    print(f"The chosen print is: {front_print}.")
+    print(f"The t-shirt size is: {size.upper()}")
+    print(f"The chosen print is: {front_print}")
 
 
 make_shirt('m', 'a.c.a.b.')
+make_shirt(size='m', front_print='a.c.a.b.')
