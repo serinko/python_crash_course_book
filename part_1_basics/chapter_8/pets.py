c14953873890a7baa92489646b82ff64c0b918fb
serinko <johanesxsalmon@riseup.net>
2022-02-20 09:22:39 +0100

    equivalent function calls

--- part_1_basics/chapter_8/pets.py
+++ part_1_basics/chapter_8/pets.py
@@ -34,3 +34,19 @@ describe_pet(pet_name='willie')
 describe_pet(pet_name='willie', animal_type='cat')
 # if using the default value, only positrional arument can be given
 describe_pet('willie')
+
+# EQUIVALENT FUNCTION CALLS
+# def describe_pet(pet_name, animal_type='dog'):
+#     """Display information about a pet."""
+#     print(f"\nI have a {animal_type}.")
+#     print(f"My {animal_type}'s name is {pet_name.title()}.")
+
+# For a dog called Willie
+describe_pet('willie')
+describe_pet(pet_name='willie')
+
+# A hamster called Harry
+describe_pet(pet_name='harry', animal_type='hamster')
+describe_pet(animal_type='hamster', pet_name='harry')
+
+# FIND THE EASIEST STYLE POSSIBLE, WHICH WORKS AND FITS YOU
