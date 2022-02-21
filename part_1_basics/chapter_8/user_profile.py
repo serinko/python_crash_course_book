90b83d16c49ef6fea762dcb8442b43eb2445a330
serinko <johanesxsalmon@riseup.net>
2022-02-21 11:36:25 +0100

    arbitrary keyword arguments

---
+++ part_1_basics/chapter_8/user_profile.py
@@ -0,0 +1,15 @@
+def build_profile(first, last, **user_info):
+    """Build a dictionary containing everything we know about user."""
+    user_info['first_name'] = first
+    user_info['last_name'] = last
+    return user_info
+
+
+user_profile = build_profile('albert', 'einstein,',
+                             location='princeton', field='physics')
+
+print(user_profile)
+
+# **parameter tells Python to create a dictionary with such name
+# The two positional parameters, which we defined as keyword in the dictionary
+# The rest is arbitraty, defined a arguments in the variable user_profile
