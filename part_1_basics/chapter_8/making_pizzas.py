feafacd1e6432e85aa12b7592df69cf31546eabf
serinko <johanesxsalmon@riseup.net>
2022-02-21 13:47:21 +0100

    import module

--- part_1_basics/chapter_8/making_pizzas.py
+++ part_1_basics/chapter_8/making_pizzas.py
@@ -1,5 +1,8 @@
 import pizza_module
 
+# importing module from the same folder
+
 pizza_module.make_pizza(33, 'ham', 'pineapple', 'cheese')
 pizza_module.make_pizza(46)
 pizza_module.make_pizza(33, 'tuna', 'bacon', 'mozzarella')
+# call the module.function(arguments)
