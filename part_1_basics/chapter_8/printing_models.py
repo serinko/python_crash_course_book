f7db8e2a1a52ba723f5bea1f62da0089362c6c8b
serinko <johanesxsalmon@riseup.net>
2022-02-21 09:13:37 +0100

    function with list and while loop

--- part_1_basics/chapter_8/printing_models.py
+++ part_1_basics/chapter_8/printing_models.py
@@ -1,16 +1,42 @@
-# Start with some designs that need to be printed.
+# # This is a way with a while loop, without any def function
+# # Start with some designs that need to be printed.
+# unprinted_designs = ['phone case', 'robot pendant', 'dodecahedron']
+# completed_models = []
+#
+# # simulateprinting each design, until none are left.
+# # Move each design to complete_models after printing.
+# while unprinted_designs:
+#     current_design = unprinted_designs.pop()
+#     print(f"Printing model: {current_design}")
+#     completed_models.append(current_design)
+#
+# # Display all completed models.
+# print("\nThe following models have been printed:")
+# for completed_model in completed_models:
+#     print(completed_model)
+
+# USING FUNCTIONS
+# We make two functions to allow flexibility
+def print_models(unprinted_designs, completed_models):
+    """
+    Simulare printing each design , until none are left.
+    Move each design to complete_models after printing.
+    """
+    while unprinted_designs:
+        current_design = unprinted_designs.pop()
+        print(f"Printing model: {current_design}")
+        completed_models.append(current_design)
+
+
+def show_completed_models(completed_models):
+    """Show all the models that were printed."""
+    print("\nThe following models have been printed:")
+    for completed_model in completed_models:
+        print(completed_model)
+
+
 unprinted_designs = ['phone case', 'robot pendant', 'dodecahedron']
 completed_models = []
 
-# simulateprinting each design, until none are left.
-# Move each design to complete_models after printing.
-while unprinted_designs:
-    current_design = unprinted_designs.pop()
-    print(f"Printing model: {current_design}")
-    completed_models.append(current_design)
-
-# Display all completed models.
-print("\nThe following models have been printed:")
-for completed_model in completed_models:
-    print(completed_model)
-
+print_models(unprinted_designs, completed_models)
+show_completed_models(completed_models)
