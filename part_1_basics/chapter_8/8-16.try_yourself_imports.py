cca3a8c6e96a80e3e8841011e44eb6b48963ece4
serinko <johanesxsalmon@riseup.net>
2022-02-22 12:03:07 +0100

    refference functions - finished excercises

--- part_1_basics/chapter_8/8-16.try_yourself_imports.py
+++ part_1_basics/chapter_8/8-16.try_yourself_imports.py
@@ -1,3 +1,13 @@
+# Importing pizza order frunctions
+
 import pizza_orders as pz
 
-pz.ch
+pz.welcoming_message()
+
+size = pz.choose_pizza_size()
+# Defines size to be used later as an arument
+
+toppings = pz.choose_pizza_toppings()
+# Defines toppings to be used later as an arument
+
+pz.make_pizza(size, toppings)
--- part_1_basics/chapter_8/pizza_orders.py
+++ part_1_basics/chapter_8/pizza_orders.py
@@ -1,64 +1,85 @@
+# MODULE
+# Contains functions to make a program for a pizza order
+
+
 def welcoming_message():
-    """Welcoming user in the cli pizza order"""
-    message = "Hello, welcome to Pizza Locco, please enter your order"
+    """Displays welcoming message in Pizza Loco order client"""
+    message = "\n\n\n\n              P I Z Z A     L O C C O               " \
+              "\n   ============================================= \n\n" \
+              "Hello, welcome to Pizza Locco, " \
+              "let's start with the order..."
     print(message)
 
 
-def choose_pizza_size(size):
-    """Prompt user to choose the size of a pizza"""
+def choose_pizza_size():
+    """Returns size of users chosen pizza"""
     active = True
     while active:
-        prompt_size = "Please choose if you want a size of 33 or 44cm pizza: "
-        prompt_size += "Enter 'q' to quickly finish your order"
+        prompt_size = \
+            "\nPlease choose if you want a size of 33 or 44cm pizza. "
+        prompt_size += \
+            "\nEnter 'q' to quickly finish your order: \n\n"
         size_user = input(prompt_size)
         if size_user.lower() == 'q':
-            print("Good bye")
+            print("No size, no pizza!!"
+                  "\nGood bye")
             quit()
 
         else:
             size_user = int(size_user)
             print(f"You have chosen pizza size {str(size_user)}.")
-            size_user = size
             active = False
-            return size_user
+
+    return size_user
 
 
 def choose_pizza_toppings():
-    """Prompt user to add toppings, append them to a list"""
-    prompt_toppings = "Please choose your toppings."
-    prompt_toppings += "Press enter after ach topping."
-    prompt_toppings += "Enter 'done' when finished."
+    """Returns a list of users chosen toppings"""
+    prompt_toppings = "\nPlease choose your toppings."
+    prompt_toppings += "\n(Press enter after ach topping."
+    prompt_toppings += "\nEnter 'q' when finished)\n"
+    prompt_toppings += "\nTOPPING: "
     active = True
-
+    tpgs = []
     while active:
         topping = input(prompt_toppings)
-        tpgs = []
-        tpgs.append(topping)
+
+        if topping == 'q':
+            print("You have chosen a pizza without any topping")
+            active = False
+        else:
+            tpgs.append(topping)
 
         if tpgs:
-            print(f"\nWe added {topping} on your pizza.")
-            prompt = "For another topping press enter, " \
-                     "or press 'q' to finish your order."
-            more = input(prompt)
-            if more.lower() == 'q':
-                active = False
+            while active:
+                print(f"\nWe added {topping} on your pizza.")
+                prompt = "For another topping press enter, " \
+                         "or press 'q' to finish your order.\n" \
+                         "\nTOPPING: "
+                more = input(prompt)
+
+                if more.lower() == 'q':
+                    active = False
+                else:
+                    tpgs.append(more)
 
         else:
-            print("You have chosen a pizza without any topping")
             active = False
 
     return tpgs
 
 
 def make_pizza(size, toppings=None):
-    """Summarize the pizza we are about to make."""
-    size_user = size
-    tpgs = toppings
-    print(f"\nThank you for your order."
+    """Displays a summary of clients pizza, size and toppings"""
+    message = "\n\n\n   ========== ORDER FINISHED ========== "
+    print(message)
+    print(f"\n\nThank you for your order. "
           f"We are preparing you pizza size {size} "
           f"with the following toppings:")
     if toppings:
         for t in toppings:
             print(f" - {t}")
     else:
-        print(" - no toppings")
+        print(" - no toppings chosen")
+
+    print("\n\nEnjoy your pizza and see you soon!!!")
