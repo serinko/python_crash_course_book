c580c5cc8bf53b8ee26d85a9657172a60e7b698a
serinko <johanesxsalmon@riseup.net>
2022-02-22 12:08:44 +0100

    excercise done

--- part_1_basics/chapter_8/8-16.try_yourself_imports.py
+++ part_1_basics/chapter_8/8-16.try_yourself_imports.py
@@ -1,13 +1,22 @@
 # Importing pizza order frunctions
+# Excercise is to use all different import approaches:
 
 import pizza_orders as pz
 
 pz.welcoming_message()
 
-size = pz.choose_pizza_size()
+from pizza_orders import choose_pizza_size
+
+size = choose_pizza_size()
 # Defines size to be used later as an argument
 
-toppings = pz.choose_pizza_toppings()
+
+from pizza_orders import choose_pizza_toppings as tpg
+
+toppings = tpg()
 # Defines toppings to be used later as an argument
 
+import pizza_orders as pz
+
 pz.make_pizza(size, toppings)
+# My most favorite - easiest to read way of importing
