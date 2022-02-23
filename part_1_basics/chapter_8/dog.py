20e9a0acdd7d18c0c7afe86822d8968e811dc86e
serinko <johanesxsalmon@riseup.net>
2022-02-23 09:05:04 +0100

    class Dog:

--- part_1_basics/chapter_8/dog.py
+++ part_1_basics/chapter_8/dog.py
@@ -13,4 +13,18 @@ class Dog:
     def roll_over(self):
         """Simulate rolling over in response to a command."""
         print(f"{self.name} rolled over!")
-           
+
+# Dog is a class
+# Any instance (particular dog) can be used for this class
+# Fuctions in a class are called methods
+# __init()__ must have the underscores so Python recognizes it automatically
+# self parameter Python reads as a refference to itself
+# PAsses automatically
+# There is two more parameters, name and age
+# both with prefix self.
+# any variable with prefix self is available to any method within the class
+# The methods sit() and roll_over() don't do much now
+# They can be extended with given instance later and describe more complex
+# object or it's behaviour
+
+ 
