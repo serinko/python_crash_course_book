dcc05c51c4d1068ab0271f57b486d31b158f481d
serinko <johanesxsalmon@riseup.net>
2022-02-23 09:59:42 +0100

    excercise 9-2_three_restaurants - finished

--- part_1_basics/chapter_8/9-1.try_yourself_restaurant.py
+++ part_1_basics/chapter_8/9-1.try_yourself_restaurant.py
@@ -15,8 +15,20 @@ class Restaurant:
         print(f"{self.name.title()} is now open!")
 
 
-restaurant = Restaurant('nagano', 'sushi')
-print(f"{restaurant.name.title()} is a new restaurant.")
-print(f"{restaurant.name.title()} is specialized in {restaurant.cuisine}.")
-restaurant.describe_restauant()
-restaurant.open_restaurant()
+restaurant_0 = Restaurant('nagano', 'sushi')
+print(f"{restaurant_0.name.title()} is a new restaurant.")
+print(f"{restaurant_0.name.title()} is specialized in {restaurant_0.cuisine}.")
+restaurant_0.describe_restauant()
+restaurant_0.open_restaurant()
+
+restaurant_1 = Restaurant('palermo', 'pizza')
+print(f"\n{restaurant_1.name.title()} is a new restaurant.")
+print(f"{restaurant_1.name.title()} is specialized in {restaurant_1.cuisine}.")
+restaurant_1.describe_restauant()
+restaurant_1.open_restaurant()
+
+restaurant_2 = Restaurant('ranger', 'burger')
+print(f"\n{restaurant_2.name.title()} is a new restaurant.")
+print(f"{restaurant_2.name.title()} is specialized in {restaurant_2.cuisine}.")
+restaurant_2.describe_restauant()
+restaurant_2.open_restaurant()
