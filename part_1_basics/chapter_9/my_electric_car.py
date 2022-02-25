1d2e50ec884a85db7bb054f0216aba9c87f133cb
serinko <johanesxsalmon@riseup.net>
2022-02-25 14:00:22 +0100

    electric_car_imported

---
+++ part_1_basics/chapter_9/my_electric_car.py
@@ -0,0 +1,10 @@
+from module_class_car import ElectricCar
+
+my_tesla = ElectricCar('tesla', 'model s', 2019)
+my_tesla.read_descriptive_name()
+
+my_tesla.battery.describe_battery()
+my_tesla.battery.get_range()
+my_tesla.battery.upgrade_battery()
+my_tesla.battery.describe_battery()
+my_tesla.battery.get_range()
