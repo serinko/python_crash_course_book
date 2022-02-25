885d4e7d67334aa9df0cae3328f7b8776e133dbb
serinko <johanesxsalmon@riseup.net>
2022-02-25 14:12:00 +0100

    import entire module

--- part_1_basics/chapter_9/my_cars.py
+++ part_1_basics/chapter_9/my_cars.py
@@ -1,3 +1,4 @@
+# Import multiple functions from a module:
 from module_class_car import Car, ElectricCar
 
 my_tesla = ElectricCar('tesla', 'roadster', 2019)
@@ -16,3 +17,12 @@ my_new_car.read_descriptive_name()
 
 my_new_car.odometer_reading = 23
 my_new_car.read_odometer()
+
+# Import entire module:
+import module_class_car as mcc
+
+my_tesla = mcc.ElectricCar('tesla', 'roadster', 2019)
+my_tesla.read_descriptive_name()
+
+my_new_car = mcc.Car('audi', 'a4', 1986)
+my_new_car.read_descriptive_name()
