9d038c167edda784cf910c05bdba82f0635cf936
serinko <johanesxsalmon@riseup.net>
2022-02-25 09:22:31 +0100

    instances as attributes

--- part_1_basics/chapter_9/electric_car.py
+++ part_1_basics/chapter_9/electric_car.py
@@ -38,6 +38,18 @@ class Car:
         self.odometer_reading += miles
 
 
+class Battery:
+    """A simple attempt to model a battery for an electric car"""
+
+    def __init__(self, battery_size=75):
+        """Initialize the battery attributes."""
+        self.battery_size = battery_size
+
+    def describe_battery(self):
+        """Print a statement describing batery size"""
+        print(f"This car has a {self.battery_size}-kWh battery.")
+
+
 class ElectricCar(Car):
     """Represents aspects of a car, specific to electric vehicles."""
 
@@ -45,12 +57,23 @@ class ElectricCar(Car):
         """Initialize attributes of the parent class."""
         super().__init__(make, model, year)
         # adding a new attribute with a default value
-        self.battery_size = 75
+        # self.battery_size = 75
+        # Commenting on it as I made a new class for battery
 
-    # And adding a new method, working with the new attribute
-    def describe_battery(self):
-        """Print a statementdescribing batery size"""
-        print(f"This car haze a {self.battery_size}-kWh battery.")
+        # adding the same attribute, defined by a class
+        self.battery = Battery()
+
+    # # And adding a new method, working with the new attribute
+    # def describe_battery(self):
+    #     """Print a statement describing batery size"""
+    #     print(f"This car haze a {self.battery_size}-kWh battery.")
+    # Commented it all as I made a new entire class battery
+
+    # Overriding a method from the parent class:
+    def fill_gas_tank(self):
+        """Electric car does not have a gas tank."""
+        print(f"Ths car does not have a gas tank.")
+    # A mistake in the book, no fill the gas tank function in class Car()
 
 
 my_tesla = ElectricCar('tesla', 'model s', 2019)
@@ -72,4 +95,22 @@ print(my_tesla.get_descriptive_name())
 # Once we have a child class, we can define new attributes and methods
 # Those will differentiate from the parrent class
 
-my_tesla.describe_battery()
+# ~ my_tesla.describe_battery()
+# This description shows that it is clearly an electric car.
+# There is no limit in the definition of the sub class
+# Attributes fitting the super class can be added there
+# Later usage of the superclass method will contain them
+
+# OVERRIDING METHODS FROM THE PARENT CLASS
+# Any method can be overridden.
+# Define the method with the same name in the child class
+# Python will disregard the parent class and use the new, same name method
+
+# INSTANCES AS ATTRIBUTES
+# Sometimes too many attributes in one class create lenghty code
+# We may create more classes instead
+# Those can be used in another programs then.
+# here it can be for example the battery.
+# Important is that the class which we reffer to later must come above!
+
+my_tesla.battery.describe_battery()
