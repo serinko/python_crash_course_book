127a86a1de1c83039888b9f6169c4236d208bd43
serinko <johanesxsalmon@riseup.net>
2022-02-25 14:34:25 +0100

    importing a module into module with aliases

--- part_1_basics/chapter_9/my_cars.py
+++ part_1_basics/chapter_9/my_cars.py
@@ -57,3 +57,24 @@ my_new_car.read_descriptive_name()
 
 my_new_car.odometer_reading = 23
 my_new_car.read_odometer()
+
+# OR USING ALIASES
+from module_class_car import Car as C
+from module_class_electirc_car import ElectricCar as EC
+
+my_tesla = EC('tesla', 'roadster', 2019)
+my_tesla.read_descriptive_name()
+
+my_tesla.battery.describe_battery()
+my_tesla.battery.get_range()
+my_tesla.battery.upgrade_battery()
+my_tesla.battery.describe_battery()
+my_tesla.battery.get_range()
+
+print("\n")
+
+my_new_car = C('audi', 'a4', 1986)
+my_new_car.read_descriptive_name()
+
+my_new_car.odometer_reading = 23
+my_new_car.read_odometer()
