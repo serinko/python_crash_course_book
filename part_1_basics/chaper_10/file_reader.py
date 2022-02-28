8ffa358f587a6393be03e1d8ce7ccec9f8e62f9d
serinko <johanesxsalmon@riseup.net>
2022-02-28 11:39:24 +0100

    Reading from a file - an entire file

--- part_1_basics/chaper_10/file_reader.py
+++ part_1_basics/chaper_10/file_reader.py
@@ -4,3 +4,10 @@
 with open('pi_digits.txt') as file_object:
     contents = file_object.read()
 print(contents)
+
+# The first line: open() accesses the file
+# argument - needs the name of the file to open
+# It looks in the directory where the program is stored
+# returns an object representing the file
+# asigns it to the file object variable
+
