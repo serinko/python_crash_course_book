0a11dc2fc0ac97a3e82c35578628eb610d0a98a9
serinko <johanesxsalmon@riseup.net>
2022-03-04 08:54:29 +0100

    append to a file

--- part_1_basics/chapter_10/write_messages.py
+++ part_1_basics/chapter_10/write_messages.py
@@ -1,6 +1,23 @@
-with open('programming.txt', 'w') as text_file:
-    text_file.write("I love programming.")
+# WRITING TO A FILE
 
-# 'w' is for write mode
-# the open() function creates the file if it did not already exist
-# BE CAREFUL !!! - if the file does exist - it will be erased and replaced
+filename = 'programming.txt'
+
+with open(filename, 'w') as text_file:
+    text_file.write("I love programming.\n")
+
+    # 'w' is for write mode
+    # the open() function creates the file if it did not already exist
+    # BE CAREFUL !!! - if the file does exist - it will be erased and replaced
+    # Python only write strings to a text file. Numbers must be str() formatted.
+
+    # WRITING MULTIPLE LINES
+    # with open(filename, 'w') as text_file:
+    #     text_file.write("I love programming.")
+    text_file.write("I love creating new games.\n")
+
+# APPENDING TO A FILE
+# Python does not erase but append to a file
+# We can modify a textfile like this:
+with open(filename, 'a') as text_file:
+    text_file.write("I also love finding meaning in large databases\n")
+    text_file.write("I love creating apps that help every day life\n")
