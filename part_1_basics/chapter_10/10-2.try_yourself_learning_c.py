237c4f2e3cb59f958dae71572fe21283e0219cfb
serinko <johanesxsalmon@riseup.net>
2022-03-04 08:07:02 +0100

    excercise finished

--- part_1_basics/chapter_10/10-2.try_yourself_learning_c.py
+++ part_1_basics/chapter_10/10-2.try_yourself_learning_c.py
@@ -7,10 +7,20 @@ learning_python_string = ''
 
 for line in lines:
     line.replace('code', 'anarchist phillosophy')
+    line = line.replace('code', 'anarchist philosophy')
     learning_python_string += line
 
-learning_python_string.replace('code', 'anarchist phillosophy')
+    # method replace() could also be done through following syntax:
+    # learning_python_string = learning_python_string.replace(
+    #    'code', 'anarchist philosophy'
+    # )
 
+# Or we with this syntax:
+# learning_python_string = learning_python_string.replace(...)
+# or alternatively - to keep the original text and replace() only for the print:
+# print(learning_python_string.replace(...))
 print(learning_python_string)
+
+# print(learning_python_string.replace('code', 'anarchist philosophy'))
 
 print("\n\n")
