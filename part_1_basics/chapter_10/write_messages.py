f5e5bbad9559dbfeeb8d5685f3a27e3a667ec955
serinko <johanesxsalmon@riseup.net>
2022-03-04 08:40:05 +0100

    be careful to erase existing file

--- part_1_basics/chapter_10/write_messages.py
+++ part_1_basics/chapter_10/write_messages.py
@@ -1,3 +1,6 @@
 with open('programming.txt', 'w') as text_file:
     text_file.write("I love programming.")
 
+# 'w' is for write mode
+# the open() function creates the file if it did not already exist
+# BE CAREFUL !!! - if the file does exist - it will be erased and replaced
