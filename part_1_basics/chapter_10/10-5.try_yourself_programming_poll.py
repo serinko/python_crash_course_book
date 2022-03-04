0060edd82c696f19f9c33c2c22f5aeb60d7c3aa0
serinko <johanesxsalmon@riseup.net>
2022-03-04 10:01:56 +0100

    flush() function

--- part_1_basics/chapter_10/10-5.try_yourself_programming_poll.py
+++ part_1_basics/chapter_10/10-5.try_yourself_programming_poll.py
@@ -11,11 +11,13 @@ file = 'programming_poll.txt'
 
 with open(file, 'a') as p_poll:
     name = input("\nWhat is your username: ")
-    p_poll.write(f"user: {name}. Reasons for programming:  ")
+    p_poll.write(f"\n\nuser: {name}. \nReasons for programming:  ")
 
     while True:
         reason = input(prompt)
         if reason.lower() != 'q':
             p_poll.write(f"\n\t- {reason}")
+            p_poll.flush()
+            # pushes an entry from memory to the disk
         else:
             quit()
