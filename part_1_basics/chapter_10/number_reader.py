1614baa81f02ecbf97358fa3ea2007e4e2d3c0e9
serinko <johanesxsalmon@riseup.net>
2022-03-05 14:58:11 +0100

    json.dump() and json.load()

--- part_1_basics/chapter_10/number_reader.py
+++ part_1_basics/chapter_10/number_reader.py
@@ -2,9 +2,17 @@
 
 import json
 
+# import the module
+
 filename = 'numbers.json'
+# defining name of the filename - name of file from data will be loaded
 
 with open(filename) as f:
     numbers = json.load(f)
+    # efyning the name of the variable (list) for the data in json
 
 print(numbers[0])
+# reading item with index [0] from the list and prints
+
+print(numbers)
+# prints the whole list
--- part_1_basics/chapter_10/number_writer.py
+++ part_1_basics/chapter_10/number_writer.py
@@ -8,8 +8,13 @@
 
 import json
 
+# import the module
 numbers = [2, 3, 5, 7, 11, 13]
 
 filename = 'numbers.json'
+# defining name of the filename - under which the data will be stored
+
 with open(filename, 'w') as f:
+    # Initiating a block to work with file as f ('w' for write)
     json.dump(numbers, f)
+    # json.dump(VARIABLE, FILE) will save it to the file defined in filename
