4b1ca46f8fba33f3f4824451142b7b434538cd1b
serinko <johanesxsalmon@riseup.net>
2022-03-05 14:38:13 +0100

    storing data

---
+++ part_1_basics/chapter_10/number_writer.py
@@ -0,0 +1,5 @@
+# json MODULE
+# allows you to DUMP simple Python data structures into a file
+# LOAD the data from the file the next time the program runs
+# It can be used to share data between differen programs
+# JSON is not specific to Python, it is language indenpendent
