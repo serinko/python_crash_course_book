025c29f8b21494f2fe828217514bda0ec2a6bce6
serinko <johanesxsalmon@riseup.net>
2022-03-05 10:00:35 +0100

    looping function of try-except-else block

--- part_1_basics/chapter_10/word_count.py
+++ part_1_basics/chapter_10/word_count.py
@@ -15,6 +15,16 @@ def count_words(filename):
         print(f"The file {filename} has about {num_words} words.")
 
 
-filename = "Alice in Wonderland.txt"
-count_words(filename)
+# filename = "Alice in Wonderland.txt"
+# count_words(filename)
 # We use the function and define the argument for its parametter
+
+# Analyzing more texts:
+filenames = [
+    "Alice in Wonderland.txt", 'siddhartha.txt', 'moby_dick.txt',
+    'harry_potter.txt'
+]
+for filename in filenames:
+    count_words(filename)
+
+
