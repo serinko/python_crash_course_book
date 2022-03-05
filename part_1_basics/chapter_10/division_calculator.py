ea56a8b4988a8e032657fd67e627baa9035bbbb0
serinko <johanesxsalmon@riseup.net>
2022-03-05 09:09:36 +0100

    simple calculator with error

--- part_1_basics/chapter_10/division_calculator.py
+++ part_1_basics/chapter_10/division_calculator.py
@@ -7,3 +7,21 @@ except ZeroDivisionError:
 
 # Because 5/0 returns ZeroDivisionError: division by zero
 # We set up an exception
+# Further code continues running because Python handled the error
+
+# USING EXCEPTIONS TO PREVENT CRASHES
+
+print("Give me two numbers and I''ll divide them.")
+print("(Enter 'q' to quit!)")
+while True:
+    first_number = input("\nFirst number: ")
+    if first_number == 'q':
+        break
+    second_number_ = input("\nSecond number: ")
+    if second_number_ == 'q':
+        break
+
+    answer = int(first_number) / int(second_number_)
+    print(answer)
+
+    
