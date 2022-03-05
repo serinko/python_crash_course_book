080cbfea29996f776744a537e01d47e9e4f6090c
serinko <johanesxsalmon@riseup.net>
2022-03-05 09:01:06 +0100

    try-except Block, ZeroDivisionError

--- part_1_basics/chapter_10/division_calculator.py
+++ part_1_basics/chapter_10/division_calculator.py
@@ -1,1 +1,9 @@
-print(5 / 0)
+# try-except blocks
+
+try:
+    print(5 / 0)
+except ZeroDivisionError:
+    print("You can't divide by zero!")
+
+# Because 5/0 returns ZeroDivisionError: division by zero
+# We set up an exception
