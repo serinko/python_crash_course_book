a02d3d861b59eaef89bd27f196adcebecaf12d89
serinko <johanesxsalmon@riseup.net>
2022-03-06 09:07:57 +0100

    first commit

--- part_1_basics/chapter_10/number_writer.py
+++ part_1_basics/chapter_10/number_writer.py
@@ -1,7 +1,7 @@
 # json MODULE
 # allows you to DUMP simple Python data structures into a file
 # LOAD the data from the file the next time the program runs
-# It can be used to share data between differen programs
+# It can be used to share data between different programs
 # JSON is not specific to Python, it is language indenpendent
 
 # WRITING TO JSON
