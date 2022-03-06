ce49f82dccd576c1bb874a3147d4dd9dfdadb9cf
serinko <johanesxsalmon@riseup.net>
2022-03-06 09:29:42 +0100

    first commit

---
+++ part_1_basics/chapter_10/remember_me.py
@@ -0,0 +1,13 @@
+# SAVING AND READING USER-GENERATED DATA
+
+import json
+
+username = input("What's your name? ")
+
+filename = 'username.json'
+
+with open(filename, 'w') as f:
+    json.dump(username, f)
+    print(f"|We'll remember you when you come back, {username}!")
+
+    
