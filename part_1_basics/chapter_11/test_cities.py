5bc636a186abb25cf3143be817287bf17095e56a
serinko <johanesxsalmon@riseup.net>
2022-03-07 12:56:45 +0100

    testing function

--- part_1_basics/chapter_11/test_cities.py
+++ part_1_basics/chapter_11/test_cities.py
@@ -12,6 +12,17 @@ class CitiesTestCase(unittest.TestCase):
         cities_formatted = get_cities_formatted('rome', 'italy')
         self.assertEqual(cities_formatted, 'Rome, Italy')
 
+    def test_city_country_population(self):
+        """Does city, country - population come out"""
+        cities_formatted = get_cities_formatted(
+            'rome',
+            'italy',
+            5000000
+        )
+        self.assertEqual(cities_formatted,
+                         'Rome, Italy - Population 5000000'
+                         )
+
 
 if __name__ == '__main__':
     unittest.main()
--- part_1_basics/chapter_11/try_yourself_11_1_city_country.py
+++ part_1_basics/chapter_11/try_yourself_11_1_city_country.py
@@ -1,6 +1,10 @@
 """Returns a single string of formatted city, country"""
 
 
-def get_cities_formatted(city, country):
-    cities_formatted = f"{city}, {country}"
+def get_cities_formatted(city, country, population=None):
+    if population:
+        cities_formatted = \
+            f"{city}, {country} - population {population}"
+    else:
+        cities_formatted = f"{city}, {country}"
     return cities_formatted.title()
