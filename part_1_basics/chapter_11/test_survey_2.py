7af6f1fa1307b1cca064d024757d87260f398f85
serinko <johanesxsalmon@riseup.net>
2022-03-08 10:51:56 +0100

    setUP() method - unittest - class

--- part_1_basics/chapter_11/test_survey_2.py
+++ part_1_basics/chapter_11/test_survey_2.py
@@ -19,13 +19,19 @@ class TestAnonymousSurvey(unittest.TestCase):
         self.my_survey = AnonymousSurvey(question)
         self.responses = ['English', 'Spanish', 'Mandarin']
 
+    # Method setUp() creates an instance of a survey
+    # Each prefixed by self to be available anywhere in the class
+    # This makes the methods easier as they do not have to make the instances
+    #
+
     # method testing is responses are storing
     def test_store_single_response(self):
         """Test that a single response is stored properly"""
         self.my_survey.store_responses(self.responses[0])
         self.assertIn(self.responses[0], self.my_survey.responses)
 
-    # We want to make sure that more responses will work - let. verify that
+    # Verifies that the frist in the list is stored correctly
+
     def test_store_three_responses(self):
         """Test that three individual responses are stored properly"""
 
@@ -36,6 +42,8 @@ class TestAnonymousSurvey(unittest.TestCase):
         # Writing another loop ensuring that each response is in responses
         for response in self.responses:
             self.assertIn(response, self.my_survey.responses)
+
+    # This method verfies that all 3 items are stored correctly 
 
 
 if __name__ == '__main__':
