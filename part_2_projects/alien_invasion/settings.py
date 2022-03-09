f996c48bef8c0581185c66dec0e9995083b32a69
serinko <johanesxsalmon@riseup.net>
2022-03-09 19:32:33 +0100

    class settings

---
+++ part_2_projects/alien_invasion/settings.py
@@ -0,0 +1,9 @@
+class Settings:
+    """A class to store all settings for Alien Invasion."""
+
+    def __init__(self):
+        """Initialize the game's settings."""
+        # Screen settings
+        self.screen_width = 2400
+        self.screen_height = 1600
+        self.bg_color = (0, 17, 26)
