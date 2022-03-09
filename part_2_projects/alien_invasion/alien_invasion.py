dadf396e3ef8f04079b5044cd426e5ba51d109b6
serinko <johanesxsalmon@riseup.net>
2022-03-09 19:14:52 +0100

    changing screen color

--- part_2_projects/alien_invasion/alien_invasion.py
+++ part_2_projects/alien_invasion/alien_invasion.py
@@ -13,13 +13,19 @@ class AlienInvasion:
         self.screen = pygame.display.set_mode((1200, 800))
         pygame.display.set_caption("Alien Invasion")
 
-    def run_game(selfself):
+        # Set the background color
+        self.bg_color = (230, 230, 230)
+
+    def run_game(self):
         """Start the maiun loop for the game"""
         while True:
             # Watch for keyboard and mouse events.
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+
+            # Redraw the screen during each pass through the loop.
+            self.screen.fill(self.bg_color)
 
             # Make the most recently drawn screen visible.
             pygame.display.flip()
@@ -29,4 +35,3 @@ if __name__ == '__main__':
     # Make a game instance, and run the game
     ai = AlienInvasion()
     ai.run_game()
-
