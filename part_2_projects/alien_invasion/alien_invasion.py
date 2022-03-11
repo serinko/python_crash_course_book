441bd86e2a410beaea604120c66f7cd5c3c3a152
serinko <johanesxsalmon@riseup.net>
2022-03-11 13:55:37 +0100

    movemnt both ways, using variable in settings

--- part_2_projects/alien_invasion/alien_invasion.py
+++ part_2_projects/alien_invasion/alien_invasion.py
@@ -37,20 +37,27 @@ class AlienInvasion:
             if event.type == pygame.QUIT:
                 sys.exit()
 
-            # event.type are pygame methods
-            # .ship refers to our ship class
             elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_RIGHT:
-                    self.ship.moving_right = True
-                elif event.key == pygame.K_LEFT:
-                    self.ship.moving_left = True
-
-
+                self._check_keydown_events(event)
             elif event.type == pygame.KEYUP:
-                if event.key == pygame.K_RIGHT:
-                    self.ship.moving_right = False
-                elif event.key == pygame.K_LEFT:
-                    self.ship.moving_left = False
+                self._check_keydown_events(event)
+
+    def _check_keydown_events(self, event):
+        """Respond to keypresses"""
+        # event.type are pygame methods
+        # .ship refers to our ship class
+
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = True
+
+
+        elif event.type == pygame.KEYUP:
+            if event.key == pygame.K_RIGHT:
+                self.ship.moving_right = False
+            elif event.key == pygame.K_LEFT:
+                self.ship.moving_left = False
 
     def _update_screen(self):
         """Update images on the screen and flip to the new screen."""
