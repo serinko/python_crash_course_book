f3cf2be3663cafc05487e4a43ec4da55d345f635
serinko <johanesxsalmon@riseup.net>
2022-03-12 19:57:07 +0100

    comments on screen size

--- part_2_projects/alien_invasion/alien_invasion.py
+++ part_2_projects/alien_invasion/alien_invasion.py
@@ -14,10 +14,12 @@ class AlienInvasion:
         pygame.init()
         self.settings = Settings()
 
+        # Run he game in fulscreen
         self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
         self.settings.screen_width = self.screen.get_rect().width
         self.settings.screen_height = self.screen.get_rect().height
 
+        # run it pre set default screen
         # self.screen = pygame.display.set_mode((
         #     self.settings.screen_width, self.settings.screen_height
         # ))
