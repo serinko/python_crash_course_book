56edd56e6628f1ff816edde196a9cbd636323051
serinko <johanesxsalmon@riseup.net>
2022-03-13 22:40:06 +0100

    super().__init__()

--- part_2_projects/alien_invasion/bullet.py
+++ part_2_projects/alien_invasion/bullet.py
@@ -1,2 +1,13 @@
 import pygame
 from pygame.sprite import Sprite
+
+
+class Bullet(Sprite):
+    """A class to manage bullets fired from the ship"""
+
+    def __init__(self, ai_game):
+    """Create a bullet object at the ship's current position"""
+    super().__init__()
+    self.screen = ai_game.screen
+    self.settings = ai_game.settings
+    
