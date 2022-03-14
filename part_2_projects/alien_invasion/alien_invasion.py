101aad163ef59ef05841d15dd3dbebeecc8e9a3e
serinko <johanesxsalmon@riseup.net>
2022-03-14 23:22:46 +0100

    bullet method added

--- part_2_projects/alien_invasion/alien_invasion.py
+++ part_2_projects/alien_invasion/alien_invasion.py
@@ -4,6 +4,7 @@ import pygame
 
 from settings import Settings
 from ship import Ship
+from bullet import Bullet
 
 
 class AlienInvasion:
@@ -48,7 +49,7 @@ class AlienInvasion:
             elif event.type == pygame.KEYDOWN:
                 self._check_keydown_events(event)
             elif event.type == pygame.KEYUP:
-                self._check_keydown_events(event)
+                self._check_keyup_events(event)
 
     def _check_keydown_events(self, event):
         """Respond to keypresses"""
@@ -61,12 +62,22 @@ class AlienInvasion:
             self.ship.moving_left = True
         elif event.key == pygame.K_q:
             sys.exit()
+        elif event.key = pygame.K_SPACE:
+            self._fire_bullet()
 
+
+    def _check_keyup_events(selef, event):
+        """Respond to key releases"""
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_RIGHT:
                 self.ship.moving_right = False
             elif event.key == pygame.K_LEFT:
                 self.ship.moving_left = False
+
+    def _fire_bullet(self):
+        """Create a new bullet and add it to the bullets group."""
+        new_bullet = Bullet(self)
+        self.bullets.add(new_bullet)
 
     def _update_screen(self):
         """Update images on the screen and flip to the new screen."""
