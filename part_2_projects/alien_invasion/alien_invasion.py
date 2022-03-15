5356a16a01c00f99778163c877db9339c12f1aa0
serinko <johanesxsalmon@riseup.net>
2022-03-15 22:42:20 +0100

    _update_bulets(self)

--- part_2_projects/alien_invasion/alien_invasion.py
+++ part_2_projects/alien_invasion/alien_invasion.py
@@ -36,14 +36,7 @@ class AlienInvasion:
         while True:
             self._check_events()
             self.ship.update()
-            self.bullets.update()
-
-            # Get rid off the dissappeared bullets
-            for bullet in self.bullets.copy():
-                if bullet.rect.bottom <= 0:
-                    self.bullets.remove(bullet)
-            # print(len(self.bullets))
-
+            self._update_bullets()
             self._update_screen()
 
     def _check_events(self):
@@ -86,6 +79,16 @@ class AlienInvasion:
         if len(self.bullets) < self.settings.bullets_allowed:
             new_bullet = Bullet(self)
             self.bullets.add(new_bullet)
+
+    def _update_bullets(self):
+        """Update position of bullets and get rid off old bullets."""
+        self.bullets.update()
+
+        # Get rid off the dissappeared bullets
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
+        # print(len(self.bullets))
 
     def _update_screen(self):
         """Update images on the screen and flip to the new screen."""
