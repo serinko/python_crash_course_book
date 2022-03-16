a7629b50d530c5b8dce789e0259eae236107266e
serinko <johanesxsalmon@riseup.net>
2022-03-16 23:00:16 +0100

    excercise 12.5 description

---
+++ part_2_projects/chapter_12/alien_01.py
@@ -0,0 +1,6 @@
+# Try It Yourse lf
+# 12-5. Sideways Shooter: Write a game that places a ship on the left side of
+# the screen and allows the player to move the ship up and down.
+# Make the ship fire a bullet that travels right across the screen
+# when the player presses the spacebar.
+# Make sure bullets are deleted once they disappear off the screen.
