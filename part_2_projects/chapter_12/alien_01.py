19945f0bd146eea015592e18fa71abeaa83d58e9
serinko <johanesxsalmon@riseup.net>
2022-03-17 21:30:05 +0100

    settings

--- part_2_projects/chapter_12/alien_01.py
+++ part_2_projects/chapter_12/alien_01.py
@@ -106,3 +106,10 @@ class AlienInvasion:
 
         # Make the most recently drawn screen visible.
         pygame.display.flip()
+
+
+if __name__ == '__main__':
+    # Make a game instance, and run the game
+    ai = AlienInvasion()
+    ai.run_game()
+
--- part_2_projects/chapter_12/settings_1.py
+++ part_2_projects/chapter_12/settings_1.py
@@ -1,1 +1,23 @@
 """Module for simple setting overview of alien_1"""
+
+
+class Settings:
+    """A class to store all settings for Alien Invasion."""
+
+    def __init__(self):
+        """Initialize the game's settings."""
+        # Screen settings
+        self.screen_width = 1920
+        self.screen_height = 1060
+
+        self.bg_color = (0, 17, 26,)
+        # Ship settings
+        # Setting the speed like this will make much easier to change it later
+        self.ship_speed = 1.5
+
+        # Bullet settings
+        self.bullet_speed = 1.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (255, 255, 255,)
+        self.bullets_allowed = 8
