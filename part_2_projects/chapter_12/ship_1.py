9560f7a7118d6e986f4a1bc4f28819bd0ba25b11
serinko <johanesxsalmon@riseup.net>
2022-03-17 21:30:56 +0100

    ship class init

--- part_2_projects/chapter_12/ship_1.py
+++ part_2_projects/chapter_12/ship_1.py
@@ -1,1 +1,34 @@
 """Module of a ship behaviour and modelation for alien_1"""
+import pygame
+from random import choice
+
+
+class Ship:
+    """A class to manage the ship"""
+
+    def __init__(self, ai_game):
+        """Initialize the ship and set its starting position."""
+        self.screen = ai_game.screen
+        self.settings = ai_game.settings
+        self.screen_rect = ai_game.screen.get_rect()
+
+        # Load the ship image and get its rect.
+        ships = []
+        ship_1 = ships.append('images/spaceship_1.bmp')
+        ship_2 = ships.append('images/spaceship_2.bmp')
+        ship = choice(ships)
+        self.image = pygame.image.load(ship)
+        self.rect = self.image.get_rect()
+
+        # Start each new ship at the bottom center of the screen
+        self.rect.midbottom = self.screen_rect.midbottom
+
+        # Store a decimal value for the ship's horizontal position.
+        # If we kept the rect as the object - it would only be able to store
+        # an integer. Variable (x) can store decimals
+        self.x = float(self.rect.x)
+
+        # Movement flags
+        # defined in the main program. If no key is pressed, the ship is stable
+        self.moving_right = False
+        self.moving_left = False
