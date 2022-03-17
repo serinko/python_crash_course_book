4e81b10f97062f33161a8bb38e9aa24b8d6784d4
serinko <johanesxsalmon@riseup.net>
2022-03-17 21:32:20 +0100

    bullet class init

--- part_2_projects/chapter_12/bullet_1.py
+++ part_2_projects/chapter_12/bullet_1.py
@@ -1,1 +1,22 @@
 """Module of a bullet behaviour and modelation for alien_1"""
+import pygame
+from pygame.sprite import Sprite
+
+
+class Bullet(Sprite):
+    """A class to manage bullets fired from the ship"""
+
+    def __init__(self, ai_game):
+        """Create a bullet object at the ship's current position"""
+        super().__init__()
+        self.screen = ai_game.screen
+        self.settings = ai_game.settings
+        self.color = self.settings.bullet_color
+
+        # create a bullet rect at (0, 0), then set the correct possition
+        self.rect = pygame.Rect(0, 0, self.settings.bullet_width,
+                                self.settings.bullet_height)
+        self.rect.midtop = ai_game.ship.rect.midtop
+
+        # store bullets position as a decimal value
+        self.y = float(self.rect.y)
