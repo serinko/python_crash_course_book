e7efbb1107388a1abf6154892b2ddd648ec89a23
serinko <johanesxsalmon@riseup.net>
2022-03-18 22:46:05 +0100

    bullet horizontal

--- part_2_projects/chapter_12/settings_1.py
+++ part_2_projects/chapter_12/settings_1.py
@@ -16,8 +16,8 @@ class Settings:
         self.ship_speed = 1.5
 
         # Bullet settings
-        self.bullet_speed = 1.0
-        self.bullet_width = 3
-        self.bullet_height = 15
+        self.bullet_speed = 3
+        self.bullet_width = 22
+        self.bullet_height = 6
         self.bullet_color = (255, 255, 255,)
-        self.bullets_allowed = 8
+        self.bullets_allowed = 20
