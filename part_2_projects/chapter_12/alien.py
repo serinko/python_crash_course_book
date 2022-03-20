38678c3e61938268663dfdbc6c23ec08ab25a245
serinko <johanesxsalmon@riseup.net>
2022-03-20 19:31:28 +0100

    def blitme

--- part_2_projects/chapter_12/alien.py
+++ part_2_projects/chapter_12/alien.py
@@ -4,17 +4,21 @@ from pygame.sprite import Sprite
     class Alien(Sprite):
         """A class to represent a single alien in the fleet."""
         def __init__(self, ai_settings, screen):
-        """Initialize the alien and set its starting position."""
-        super(Alien, self).__init__()
-        self.screen = screen
-        self.ai_settings = ai_settings
+            """Initialize the alien and set its starting position."""
+            super(Alien, self).__init__()
+            self.screen = screen
+            self.ai_settings = ai_settings
 
 
-        # Load the alien image and set its rect attribute.
-        self.image = pygame.image.load('images/alien.bmp')
-        self.rect = self.image.get_rect()
+            # Load the alien image and set its rect attribute.
+            self.image = pygame.image.load('images/alien.bmp')
+            self.rect = self.image.get_rect()
 
-        # Start each new alien near the top left of the screen.
-        self.rect.x = self.rect.width
-        self.rect.y = self.rect.height
-        # Store the alien's exact positio
+            # Start each new alien near the top left of the screen.
+            self.rect.x = self.rect.width
+            self.rect.y = self.rect.height
+            # Store the alien's exact position
+
+            def blitme(self):
+                """Draw the alien at its current location."""
+                self.screen.blit(self.image, self.rect)
