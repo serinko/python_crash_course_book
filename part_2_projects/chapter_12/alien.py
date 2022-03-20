f1f6375940c2b83cca30d7e914b081a189640864
serinko <johanesxsalmon@riseup.net>
2022-03-20 19:27:49 +0100

    alien class

---
+++ part_2_projects/chapter_12/alien.py
@@ -0,0 +1,12 @@
+import pygame
+from pygame.sprite import Sprite
+
+    class Alien(Sprite):
+        """A class to represent a single alien in the fleet."""
+        def __init__(self, ai_settings, screen):
+        """Initialize the alien and set its starting position."""
+        super(Alien, self).__init__()
+        self.screen = screen
+        self.ai_settings = ai_settings
+
+        
