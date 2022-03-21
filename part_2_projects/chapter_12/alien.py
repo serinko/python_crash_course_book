76853dc393f3116b9a9a13b117949080eb1d179e
serinko <johanesxsalmon@riseup.net>
2022-03-21 23:48:11 +0100

    class alien

--- part_2_projects/chapter_12/alien.py
+++ part_2_projects/chapter_12/alien.py
@@ -9,3 +9,16 @@ class Alien(Sprite):
         """Initialize the alien and set it's starting position"""
         super().__init__()
         self.screen = ai_game.screen
+
+        # Load the alien image and set its rect attribute
+        self.image = pygame.load("images/alien.bmp")
+        self.rect = self.image_get.rect()
+
+        # Start each new line near to the top left of the screen
+        self.rect.x = self.rect.width
+        self.rect.y = self.rect.height
+
+        # Store alien exact horizontal position
+        self.x = float(self.rect.x)
+
+        
