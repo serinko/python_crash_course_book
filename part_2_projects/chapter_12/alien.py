b0fc172cc5338c2df7b1702903c6e1bc3a33b5f5
serinko <johanesxsalmon@riseup.net>
2022-03-21 23:40:26 +0100

    deleting non working code

--- part_2_projects/chapter_12/alien.py
+++ part_2_projects/chapter_12/alien.py
@@ -10,13 +10,3 @@ class Alien(Sprite):
         super(Alien, self).__init__()
         self.screen = screen
         self.ai_settings = ai_settings
-
-        # Load the alien image and set its rect attribute.
-        self.image = pygame.image.load('images/alien.bmp')
-        self.rect = self.image.get_rect()
-
-        # Start each new alien near the top left of the screen.
-        self.rect.x = self.rect.width
-        self.rect.y = self.rect.height
-
-        # Store the alien's exact position
