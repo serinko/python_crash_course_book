36a16d936f6252c46f287068b0fc06a7067222c9
serinko <johanesxsalmon@riseup.net>
2022-03-22 09:11:35 +0100

    fixing bugs

--- part_2_projects/alien_invasion/alien.py
+++ part_2_projects/alien_invasion/alien.py
@@ -11,8 +11,8 @@ class Alien(Sprite):
         self.screen = ai_game.screen
 
         # Load the alien image and set its rect attribute
-        self.image = pygame.load("images/alien.bmp")
-        self.rect = self.image_get.rect()
+        self.image = pygame.image.load("images/alien.bmp")
+        self.rect = self.image.get_rect()
 
         # Start each new line near to the top left of the screen
         self.rect.x = self.rect.width
@@ -20,4 +20,3 @@ class Alien(Sprite):
 
         # Store alien exact horizontal position
         self.x = float(self.rect.x)
-
