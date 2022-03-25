2525a9c1e081a0f1f9dff57e6abe47ccc1a3371d
serinko <johanesxsalmon@riseup.net>
2022-03-25 14:03:24 +0100

    Settings

--- part_2_projects/chapter_12/try_yourself_stars.py
+++ part_2_projects/chapter_12/try_yourself_stars.py
@@ -1,23 +1,20 @@
 import sys
 
 import pygame
+from pygame.sprite import Sprite
 
 
-class Star:
+class Star(Sprite):
     def __init__(self, StarSky):
         """Initialize the alien and set it's starting position"""
         super().__init__()
         self.screen = StarSky.screen
 
         # Load the alien image and set its rect attribute
-        self.image = pygame.image.load("images/alien.bmp")
+        self.image = pygame.image.load("images/star.bmp")
         self.rect = self.image.get_rect()
-
-        # Start each new line near to the top left of the screen
         self.rect.x = self.rect.width
         self.rect.y = self.rect.height
-
-        # Store alien exact horizontal position
         self.x = float(self.rect.x)
 
 
@@ -38,51 +35,40 @@ class StarSky:
         """Initialize the game and create game resources."""
         pygame.init()
         self.settings = Settings()
-
-        # Run he game in full screen
         self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
         self.settings.screen_width = self.screen.get_rect().width
         self.settings.screen_height = self.screen.get_rect().height
-
         pygame.display.set_caption("Star Sky")
-
         self.stars = pygame.sprite.Group()
-
         self._create_sky()
-
-        # Set the background color
         self.bg_color = (0, 17, 26)
 
+    def _check_events(self):
+        """Respond to key-presses and mouse events"""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
     def run(self):
-        self._update_screen()
+        while True:
+            self._check_events()
+            self._update_screen()
 
     def _create_sky(self):
         """Create the fleet of stars"""
-
         star = Star(self)
-        # self.aliens.add(alien)
         star_width, star_height = star.rect.size
-        # counts # fitting aliens per row
         available_space_x = self.settings.screen_width - (2 * star_width)
-        number_star_x = available_space_x // (2 * star_width)
-        # // sgn is a floor division, drops off all the reminder.
-        # Always returns an integer
-
-        # Determine the number of aliens
-
+        number_stars_x = available_space_x // (2 * star_width)
         available_space_y = \
             (self.settings.screen_height - (3 * star_height))
-
         number_rows = available_space_y // (2 * star_height)
 
-        # Create the full fleet of aliens
+        # Create the full fleet of stars
         for row_number in range(number_rows):
-            # Create the first row of aliens
-            for star_number in range(number_star_x):
+            # Create the first row of stars
+            for star_number in range(number_stars_x):
                 self._create_star(star_number, row_number)
-
-                # Two nested loops when one make a row from an alien
-                # Another makes a plot of rows from one row
 
     def _create_star(self, star_number, row_number):
         """Create alien and place it in the row"""
@@ -91,7 +77,7 @@ class StarSky:
         star.x = star_width + 2 * star_width * star_number
         star.rect.x = star.x
         star.rect.y = star_height + 2 * star.rect.height * row_number
-        self.star.add(star)
+        self.stars.add(star)
         # Adds to the group aliens in Sprite (in __init__)
 
     def _update_screen(self):
