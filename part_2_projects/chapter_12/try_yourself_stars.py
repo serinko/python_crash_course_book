b098045b3953d1df75bac3fff01e1e212cfc8cad
serinko <johanesxsalmon@riseup.net>
2022-03-25 14:05:27 +0100

    q = sys.exit()

--- part_2_projects/chapter_12/try_yourself_stars.py
+++ part_2_projects/chapter_12/try_yourself_stars.py
@@ -48,6 +48,9 @@ class StarSky:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_q:
+                    sys.exit()
 
     def run(self):
         while True:
