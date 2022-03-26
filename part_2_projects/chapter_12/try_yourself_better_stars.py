61d2d40ea7be82d388aefdc973f84d09dd3b9d28
serinko <johanesxsalmon@riseup.net>
2022-03-26 10:25:59 +0100

    building random star screen

--- part_2_projects/chapter_12/try_yourself_better_stars.py
+++ part_2_projects/chapter_12/try_yourself_better_stars.py
@@ -1,1 +1,16 @@
-git
+import sys
+import pygame
+from pygame.sprite import Sprite
+
+
+class Star(Sprite):
+    def __init__(self, StarSky):
+        """Initialize the alien and set it's starting position"""
+        super().__init__()
+        self.screen = StarSky.screen
+
+        # Load the alien image and set its rect attribute
+        self.image = pygame.image.load("images/a_star.bmp")
+        self.rect = self.image.get_rect()
+        self.rect.x = self.rect.width
+        self.rect.y = self.rect.height
