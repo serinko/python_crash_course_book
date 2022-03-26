811caf3242fb5060eb66a25da8ef4f2867ce3c07
serinko <johanesxsalmon@riseup.net>
2022-03-26 10:26:11 +0100

    building random star screen

--- part_2_projects/chapter_12/try_yourself_better_stars.py
+++ part_2_projects/chapter_12/try_yourself_better_stars.py
@@ -14,3 +14,28 @@ class Star(Sprite):
         self.rect = self.image.get_rect()
         self.rect.x = self.rect.width
         self.rect.y = self.rect.height
+
+
+class StarSky:
+    """Overall class to manage game assets and behaviour"""
+
+    def __init__(self):
+        """Initialize the game and create game resources."""
+        pygame.init()
+        self.settings = Settings()
+        self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+        self.settings.screen_width = self.screen.get_rect().width
+        self.settings.screen_height = self.screen.get_rect().height
+        pygame.display.set_caption("Star Sky")
+        self.stars = pygame.sprite.Group()
+        self._create_sky()
+        self.bg_color = (0, 17, 26)
+
+    def _check_events(self):
+        """Respond to key-presses and mouse events"""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_q:
+                    sys.exit()
