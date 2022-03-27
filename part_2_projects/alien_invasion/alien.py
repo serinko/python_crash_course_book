b6bfcffa8102fc5eed0f1f471f4941685267f7f4
serinko <johanesxsalmon@riseup.net>
2022-03-27 13:59:04 +0200

    check edges

--- part_2_projects/alien_invasion/alien.py
+++ part_2_projects/alien_invasion/alien.py
@@ -33,5 +33,5 @@ class Alien(Sprite):
         """Return True if alien is at the dge of the screen"""
         screen_rect = self.screen.get_rect()
 
-        if self.rect >= screen_rect.right or self.rect <= 0:
+        if self.rect.right >= screen_rect.right or self.rect.left <= 0:
             return True
