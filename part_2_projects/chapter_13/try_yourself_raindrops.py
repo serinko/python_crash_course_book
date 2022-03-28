22b168ea14be804ce2474330f3e796d310841881
serinko <johanesxsalmon@riseup.net>
2022-03-28 23:49:20 +0200

    class RainDrop

--- part_2_projects/chapter_13/try_yourself_raindrops.py
+++ part_2_projects/chapter_13/try_yourself_raindrops.py
@@ -1,3 +1,16 @@
 import sys
 import pygame
 from pygame.sprite import Sprite
+
+
+class RainDrop(Sprite):
+    def __init__(self, StarSky):
+        """Initialize the alien and set it's starting position"""
+        super().__init__()
+        self.screen = StarSky.screen
+
+        # Load the alien image and set its rect attribute
+        self.image = pygame.image.load("images/a_star.bmp")
+        self.rect = self.image.get_rect()
+        self.rect.x = self.rect.width
+        self.rect.y = self.rect.height
