05ff14f9a5df1069bf1b3910bf20e3612327f0fe
serinko <johanesxsalmon@riseup.net>
2022-03-29 10:49:38 +0200

    developing rain

--- part_2_projects/chapter_13/try_yourself_raindrops.py
+++ part_2_projects/chapter_13/try_yourself_raindrops.py
@@ -10,8 +10,6 @@ class RainDrop(Sprite):
         self.screen = StarSky.screen
         self.settings = Settings()
 
-
-
         # Load the alien image and set its rect attribute
         self.image = pygame.image.load("images/raindrop.bmp")
         self.rect = self.image.get_rect()
@@ -20,16 +18,17 @@ class RainDrop(Sprite):
 
     def update(self):
         """Move the alien to the right and left"""
-        self.x += (self.settings.alien_speed * \
-                   self.settings.fleet_direction)
+        self.y += (self.settings.raindrop_speed * \
+                   self.settings.rain_direction)
 
-        self.rect.x = self.x
+        self.rect.y = self.y
 
     def check_edges(self):
         """Return True if alien is at the dge of the screen"""
         screen_rect = self.screen.get_rect()
 
-        if self.rect.right >= screen_rect.right
+        if self.rect.top >= screen_rect.bottom:
+            return True
 
 
 class Settings():
@@ -39,7 +38,6 @@ class Settings():
         self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
 
         self.bg_color = (0, 17, 26,)
-
 
         # Raindrop settings
         self.raindrop_speed = 1.0
@@ -59,8 +57,8 @@ class Rain:
         self.settings.screen_width = self.screen.get_rect().width
         self.settings.screen_height = self.screen.get_rect().height
         pygame.display.set_caption("Star Sky")
-        self.stars = pygame.sprite.Group()
-        self._create_sky()
+        self.raindrops = pygame.sprite.Group()
+        self._create_rain()
         self.bg_color = (0, 17, 26)
 
     def _check_events(self):
@@ -75,31 +73,32 @@ class Rain:
     def run(self):
         while True:
             self._check_events()
+            self._update_sky()
             self._update_screen()
 
-    def _create_sky(self):
+    def _create_rain(self):
         """Create the fleet of stars"""
-        star = Star(self)
-        star_width, star_height = star.rect.size
+        raindrop = RainDrop(self)
+        raindrop_width, raindrop_height = raindrop.rect.size
         available_space_x = self.settings.screen_width
-        number_stars_x = available_space_x // star_width
+        number_raindrop_x = available_space_x // raindrop_width
         available_space_y = self.settings.screen_height
-        number_rows = available_space_y // star_height
+        number_rows = available_space_y // raindrop_height
 
         # Create the full fleet of stars
         for row_number in range(number_rows):
             # Create the first row of stars
-            for star_number in range(number_stars_x):
-                self._create_star(star_number, row_number)
+            for star_number in range(number_raindrop_x):
+                self._create_raindrop(raindrop_number, row_number)
 
-    def _create_star(self, star_number, row_number):
+    def _create_raindrop(self, raindrop_number, row_number):
         """Create alien and place it in the row"""
-        star = Star(self)
-        star_width, star_height = star.rect.size
-        star.x = 1.18 * star_width * star_number
-        star.rect.x = star.x
-        star.rect.y = 1.18 * star.rect.height * row_number
-        self.stars.add(star)
+        raindrop = RainDrop(self)
+        raindrop_width, raindrop_height = raindrop.rect.size
+        raindrop.x = 1.18 * raindrop_width * raindrop_number
+        raindrop.rect.x = raindrop.x
+        raindrop.rect.y = 1.18 * raindrop.rect.height * row_number
+        self.raindrops.add(raindrop)
         # Adds to the group aliens in Sprite (in __init__)
 
     def _update_screen(self):
@@ -107,7 +106,7 @@ class Rain:
         # Redraw the screen during each pass through the loop.
         self.screen.fill(self.settings.bg_color)
 
-        self.stars.draw(self.screen)
+        self.raindrops.draw(self.screen)
 
         # Make the most recently drawn screen visible.
         pygame.display.flip()
