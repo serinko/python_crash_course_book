fbde31532dda67963f2240a7f8308faae3f81738
serinko <johanesxsalmon@riseup.net>
2022-04-03 12:30:59 +0200

    high core centered

--- part_2_projects/alien_invasion/scoreboard.py
+++ part_2_projects/alien_invasion/scoreboard.py
@@ -56,4 +56,6 @@ class Scoreboard:
         )
 
         # Center the high score at the top of the screen.
-        
+        self.high_score_rect = self.high_score_image.get_rect()
+        self.high_score_rect.centerx = self.screen_rect.centerx
+        self.high_score_rect.top = self.score_rect.top
