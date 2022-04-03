3fd69eb4def00763ae267ada9dec58eec1c9782c
serinko <johanesxsalmon@riseup.net>
2022-04-03 10:13:58 +0200

    reset the game

--- part_2_projects/alien_invasion/game_stats.py
+++ part_2_projects/alien_invasion/game_stats.py
@@ -4,11 +4,11 @@ class GameStats:
     def __init__(self, ai_game):
         """Initialize statistics."""
         self.settings = ai_game.settings
-        self._reset_stats()
+        self.reset_stats()
 
         # Start alien invasion in an inactive state
         self.game_active = False
 
-    def _reset_stats(self):
+    def reset_stats(self):
         """Initialize statistics that can change during the game."""
         self.ships_left = self.settings.ship_limit
