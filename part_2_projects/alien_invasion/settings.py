7338700efc9d7cd4cb978fdbd68a28b471c879f7
serinko <johanesxsalmon@riseup.net>
2022-04-03 10:46:36 +0200

    def initialize_dynamic_settings

--- part_2_projects/alien_invasion/settings.py
+++ part_2_projects/alien_invasion/settings.py
@@ -7,27 +7,40 @@ class Settings:
     """A class to store all settings for Alien Invasion."""
 
     def __init__(self):
-        """Initialize the game's settings."""
+        """Initialize the game's static settings."""
         # Screen settings
         self.screen_width = 1920
         self.screen_height = 1060
         # self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
 
         self.bg_color = (0, 17, 26,)
+
         # Ship settings
-        # Setting the speed like this will make much easier to change it later
-        self.ship_speed = 7
         self.ship_limit = 3
 
         # Bullet settings
-        self.bullet_speed = 5
         self.bullet_width = 3
         self.bullet_height = r.randrange(15, 40)
         self.bullet_color = (255, 255, 255,)
         self.bullets_allowed = 20
 
         # Alien settings
-        self.alien_speed = 1.0
         self.fleet_drop_speed = 20
+
+        # How quickly the fleet speeds up
+        self.speedup_scale = 1.1
+
+        self.initialize_dynamic_settings()
+
+    def initialize_dynamic_settings(self):
+        """Initialize setting that change during the game"""
+
+        # Setting the speed like this will make much easier to change it later
+        self.ship_speed = 7
+
+        self.bullet_speed = 5
+        self.alien_speed = 1.0
+
         # Fleet direction of 1 represents right; -1 represents left
         self.fleet_direction = 1
+
