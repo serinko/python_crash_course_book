01f4c99c51c5fa2d641f094c7deff3487a21352d
serinko <johanesxsalmon@riseup.net>
2022-04-03 11:36:45 +0200

    show score

--- part_2_projects/alien_invasion/scoreboard.py
+++ part_2_projects/alien_invasion/scoreboard.py
@@ -38,4 +38,4 @@ class Scoreboard:
 
     def show_score(self):
         """Draw score to the screen"""
-        self.screen.blit(self.score, self.score_rect)
+        self.screen.blit(self.score_image, self.score_rect)
