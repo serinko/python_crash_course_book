347d8d6f4a3eac8904e3f1162cc3c8ca74b99c90
serinko <johanesxsalmon@riseup.net>
2022-04-07 11:05:40 +0200

    simple graphs

--- part_2_projects/data_visualisation/mpl_squares.py
+++ part_2_projects/data_visualisation/mpl_squares.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 squares = [1, 4, 9, 16, 25]
 
 fig, ax = plt.subplots()
+ax.plot(squares, linewidth=3)
 
 # set chart title and label axes
 ax.set_title("Square Numbers", fontsize=24)
