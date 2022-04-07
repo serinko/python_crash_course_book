df426d2ef8e1204170a4913b709b395149a8b03a
serinko <johanesxsalmon@riseup.net>
2022-04-07 10:50:35 +0200

    simple graphs

--- part_2_projects/data_visualisation/mpl_squares.py
+++ part_2_projects/data_visualisation/mpl_squares.py
@@ -3,6 +3,14 @@ import matplotlib.pyplot as plt
 squares = [1, 4, 9, 16, 25]
 
 fig, ax = plt.subplots()
+
+# set chart title and label axes
+ax.st_title("Square Numbers", fontsize=24)
+ax.set_xlabel("Value", fontsize=24)
+ax.set_ylabel("Square of Value", fontsize=24)
 ax.plot(squares)
+
+# set size of thick labels
+ax.tick_prams(axis='both', labelsize=14)
 
 plt.show()
