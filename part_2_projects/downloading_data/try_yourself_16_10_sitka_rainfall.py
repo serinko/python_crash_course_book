4c9d3eeee3398292fec5299854def2f721d93626
serinko <johanesxsalmon@riseup.net>
2022-04-13 13:55:19 +0200

    not functioning int()

--- part_2_projects/downloading_data/try_yourself_16_10_sitka_rainfall.py
+++ part_2_projects/downloading_data/try_yourself_16_10_sitka_rainfall.py
@@ -12,34 +12,32 @@ with open(filename) as f:
     # print(header_row)
     # # Outcome is comma-separated line
 
-    for index, column_header in enumerate(header_row):
-        print(index, column_header)
-    # Outcome is list of index and value of items in the header
-#
-#     # Get dates and high and low temperatures from this file.
-#     dates, highs, lows = [], [], []
-#     for row in reader:
-#         current_date = datetime.strptime(row[2], '%Y-%m-%d')
-#         high = int(row[5])
-#         low = int(row[6])
-#         dates.append(current_date)
-#         highs.append(high)
-#         lows.append(low)
-#     # The loop started on line 2 as we moved before to the next() once
-#
-# # Plot the high and low temperatures.
-# plt.style.use('seaborn')
-# fig, ax = plt.subplots()
-# ax.plot(dates, highs, c='red', alpha=0.5)
+    # for index, column_header in enumerate(header_row):
+    #     print(index, column_header)
+    # # Outcome is list of index and value of items in the header
+
+    # Get dates and the rainfall
+    dates, rainfall = [], []
+    for row in reader:
+        current_date = datetime.strptime(row[2], '%Y-%m-%d')
+        rain = int(row[3])
+        dates.append(current_date)
+        rainfall.append(rain)
+    # The loop started on line 2 as we moved before to the next() once
+
+# Plot the high and low temperatures.
+plt.style.use('seaborn')
+fig, ax = plt.subplots()
+ax.plot(dates, rain, c='purple', alpha=0.8)
 # ax.plot(dates, lows, c='blue', alpha=0.5)
 # ax.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
-#
-# # Format plot.
-# ax.set_title("Daily high and low temperatures - 2018", fontsize=24)
-# ax.set_xlabel("", fontsize=16)
-# fig.autofmt_xdate()
-# # draws the date labels diagonally to prevent them from overlapping
-# ax.set_ylabel("Temperature (F)", fontsize=16)
-# ax.tick_params(axis='both', which='major', labelsize=16)
-#
-# plt.show()
+
+# Format plot.
+ax.set_title("Daily rainfall - 2018, Sitka", fontsize=24)
+ax.set_xlabel("", fontsize=16)
+fig.autofmt_xdate()
+# draws the date labels diagonally to prevent them from overlapping
+ax.set_ylabel("Rainfall", fontsize=16)
+ax.tick_params(axis='both', which='major', labelsize=16)
+
+plt.show()
