f73a0cdf76a26823b7dd8f652cbf7af8b7094b1a
serinko <johanesxsalmon@riseup.net>
2022-04-16 10:43:37 +0200

    formating new data set

---
+++ part_2_projects/downloading_data/try_yourself_16_8_recent_earthquakes.py
@@ -0,0 +1,52 @@
+import json
+
+from plotly.graph_objs import Scattergeo, Layout
+from plotly import offline
+
+# Explore the structure of the data
+filename = 'data/mapping_global_data_sets/data/significant_month.geojson.json'
+with open(filename) as f:
+    all_eq_data = json.load(f)
+
+readable_file = 'data/mapping_global_data_sets/data/significant_month.geojson.json'
+with open(readable_file, 'w') as f:
+    json.dump(all_eq_data, f, indent=4)
+# formatting the data with a proper data structure
+#
+# all_eq_dicts = all_eq_data['features']
+# name = all_eq_data['metadata']['title']
+# # print(len(all_eq_dicts))
+#
+# # Pulling the magnitudes
+# mags, lons, lats, hover_texts = [], [], [], []
+# for eq_dict in all_eq_dicts:
+#     mag = eq_dict['properties']['mag']
+#     lon = eq_dict['geometry']['coordinates'][0]
+#     lat = eq_dict['geometry']['coordinates'][1]
+#     title = eq_dict['properties']['title']
+#     mags.append(mag)
+#     lons.append(lon)
+#     lats.append(lat)
+#     hover_texts.append(title)
+#
+# # Map the earthquakes
+# # data = [Scattergeo(lon=lons, lat=lats)]
+# # alternative:
+# data = [{
+#     'type': 'scattergeo',
+#     'lon': lons,
+#     'lat': lats,
+#     'text': hover_texts,
+#     'marker': {
+#         'size': [5 * mag for mag in mags],
+#         'color': mags,
+#         'colorscale': 'Viridis',
+#         'reversescale': True,
+#         'colorbar': {'title': 'Magnitude'}
+#     }
+# }]
+#
+# my_layout = Layout(title=name)
+#
+# fig = {'data': data, 'layout': my_layout}
+# offline.plot(fig, filename='global_earthquakes.html')
