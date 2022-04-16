3a6055717712174ea7e51c00a7686b1a14ac1fb1
serinko <johanesxsalmon@riseup.net>
2022-04-16 12:44:50 +0200

    world fires csv - finished

--- part_2_projects/downloading_data/try_yourself_16_9_world_fires.py
+++ part_2_projects/downloading_data/try_yourself_16_9_world_fires.py
@@ -18,41 +18,47 @@ with open(filename) as f:
 
     brigthnes, lons, lats, hover_dates = [], [], [], []
     for row in reader:
+
         bright_idx = header_row.index('bright_ti4')
         lon_index = header_row.index('longitude')
         lat_index = header_row.index('latitude')
         date_index = header_row.index('acq_date')
+        time_index = header_row.index('acq_time')
+
         try:
             bright = float(row[bright_idx])
             lon = float(row[lon_index])
             lat = float(row[lat_index])
-            hover_date = row[5]
-        except ValueError:
+            date_time = f"{row[date_index]}, {row[time_index]}"
+            hover_date_time = \
+                datetime.strptime(date_time, '%Y-%m-%d, %H%M')
+
+        except IndexError:
             print(f"Missing data")
         else:
             brigthnes.append(bright)
             lons.append(lon)
             lats.append(lat)
-            hover_dates.append(hover_date)
+            hover_dates.append(hover_date_time)
 
 # Map the earthquakes
 # data = [Scattergeo(lon=lons, lat=lats)]
 # alternative:
 data = [{
     'type': 'scattergeo',
-    'lon': lons,
-    'lat': lats,
-    'text': hover_dates,
+    'lon': lons[:500],
+    'lat': lats[:500],
+    'text': hover_dates[:500],
     'marker': {
-        'size': [5 * bright for bright in brigthnes],
-        'color': mags,
+        'size': 20,
+        'color': brigthnes,
         'colorscale': 'Hot',
-        'reversescale': False,
-        'colorbar': {'title': 'Magnitude'}
+        'reversescale': True,
+        'colorbar': {'title': 'Brightnes'}
     }
 }]
 
 my_layout = Layout(title='World Fires 7 Days')
 
 fig = {'data': data, 'layout': my_layout}
-offline.plot(fig, filename='global_earthquakes.html')
+offline.plot(fig, filename='global_fires.html')
