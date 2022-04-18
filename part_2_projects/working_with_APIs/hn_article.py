8e02e6425ffcbed06166fe806cd7c705f6573586
serinko <johanesxsalmon@riseup.net>
2022-04-18 11:42:20 +0200

    Hacker NEws API

--- part_2_projects/working_with_APIs/hn_article.py
+++ part_2_projects/working_with_APIs/hn_article.py
@@ -3,7 +3,7 @@ import json
 
 # Make an API call and store the response
 url = \
-    'https://hacker-news.firebaseio.com/v0/item/item/19155826.json'
+    'https://hacker-news.firebaseio.com/v0/item/19155826.json'
 r = requests.get(url)
 print(f"Status code: {r.status_code}")
 
