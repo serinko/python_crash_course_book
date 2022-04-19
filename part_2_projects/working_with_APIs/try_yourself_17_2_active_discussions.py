ec380c95ddb86075637d1dc3facd23fc8c9b1cfb
serinko <johanesxsalmon@riseup.net>
2022-04-19 10:57:31 +0200

    excercise finished

--- part_2_projects/working_with_APIs/try_yourself_17_2_active_discussions.py
+++ part_2_projects/working_with_APIs/try_yourself_17_2_active_discussions.py
@@ -8,6 +8,9 @@
 
 from operator import itemgetter
 import requests
+import json
+from plotly.graph_objs import Bar
+from plotly import offline
 
 # Make an API call and store the response
 url = \
@@ -18,6 +21,7 @@ print(f"Status code: {r.status_code}")
 # Process information about each submission
 submission_ids = r.json()
 submission_dicts = []
+comments, titles_links = [], [],
 for submission_id in submission_ids[:30]:
     # Make a separate API call for each submission
     url = f"https://hacker-news.firebaseio.com/v0/item/{submission_id}.json"
@@ -25,21 +29,70 @@ for submission_id in submission_ids[:30]:
     print(f"id: {submission_id}\tstatus: {r.status_code}")
     response_dict = r.json()
 
-    # Build a dictionary for each article
-    submission_dict = {
-        'title': response_dict['title'],
-        'hn_link': f"http://news.ycombinator.com/item?id={submission_id}",
-        'comments': response_dict['descendants'],
-    }
-    submission_dicts.append(submission_dict)
+    try:
+        # Build a dictionary for each article
+        submission_dict = {
+            'title': response_dict['title'],
+            'hn_link': f"http://news.ycombinator.com/item?id={submission_id}",
+            'comments': response_dict['descendants'],
+        }
 
-submission_dicts = sorted(
+    except KeyError:
+        print(f"ID: {submission_id}\tstatus: Key Error")
+
+    else:
+        # comment = submission_dict['comments']
+        # title = submission_dict['title']
+        # url = f"http://news.ycombinator.com/item?id={submission_id}"
+        # title_link = f"<a href='{url}'>{title}</a>"
+        # comments.append(comment)
+        # titles_links.append(title_link)
+        submission_dicts.append(submission_dict)
+
+submission_dicts_sorted = sorted(
     submission_dicts,
     key=itemgetter('comments'),
     reverse=True,
 )
 
-for submission_dict in submission_dicts:
-    print(f"\nTitle: {submission_dict['title']}")
-    print(f"Discussion link: {submission_dict['hn_link']}")
-    print(f"Comments: {submission_dict['comments']}")
+for dict in submission_dicts_sorted:
+    comment = dict['comments']
+    title = dict['title']
+    url = dict['hn_link']
+    title_link = f"<a href='{url}'>{title}</a>"
+    comments.append(comment)
+    titles_links.append(title_link)
+
+# Make visualisation
+data = [{
+    'type': 'bar',
+    'x': titles_links,
+    'y': comments,
+    'hovertext': titles_links,
+    'marker': {
+        'color': 'rgb(60,100,150)',
+        'line': {
+            'width': 1.5,
+            'color': 'rgb(25,25,25)',
+        },
+    },
+    'opacity': 0.6,
+}]
+
+my_layout = {
+    'title': f'Most-commented articles on Hacker News',
+    'titlefont': {'size': 30},
+    'xaxis': {
+        'title': 'Repository',
+        'titlefont': {'size': 24},
+        'tickfont': {'size': 14},
+    },
+    'yaxis': {
+        'title': 'Stars',
+        'titlefont': {'size': 24},
+        'tickfont': {'size': 14},
+    }
+}
+
+fig = {'data': data, 'layout': my_layout}
+offline.plot(fig, filename=f'hn_commented_articles.html')
